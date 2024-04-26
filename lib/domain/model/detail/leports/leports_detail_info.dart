8f94ba4a8cf9c0e32085f40aa93312e70a110ca5
Yejin.Shin <mingmi_27@naver.com>
2024-04-26 19:21:20 +0900

    feat: Leports 폴더 Detail 모델 추가

---
+++ lib/domain/model/detail/leports/leports_detail_info.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'leports_detail_info.freezed.dart';
+part 'leports_detail_info.g.dart';
+
+@freezed
+class LeportsDetailInfo with _$LeportsDetailInfo {
+  const factory LeportsDetailInfo({
+    required String infoName,
+    required String infoText,
+  }) = _LeportsDetailInfo;
+
+  factory LeportsDetailInfo.fromJson(Map<String, Object?> json) =>
+      _$LeportsDetailInfoFromJson(json);
+}
---
+++ lib/domain/model/detail/leports/leports_detail_info.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'leports_detail_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$LeportsDetailInfoImpl _$$LeportsDetailInfoImplFromJson(
+        Map<String, dynamic> json) =>
+    _$LeportsDetailInfoImpl(
+      infoName: json['infoName'] as String,
+      infoText: json['infoText'] as String,
+    );
+
+Map<String, dynamic> _$$LeportsDetailInfoImplToJson(
+        _$LeportsDetailInfoImpl instance) =>
+    <String, dynamic>{
+      'infoName': instance.infoName,
+      'infoText': instance.infoText,
+    };
