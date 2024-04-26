bae687889ff397973fd6be5798bab21d07ff3478
chojja7188 <shoun7188@naver.com>
2024-04-26 22:48:08 +0900

    feat: 코스 모델 수정

--- lib/domain/model/detail/course/course_detail.g.dart
+++ lib/domain/model/detail/course/course_detail.g.dart
@@ -10,14 +10,10 @@ _$CourseDetailImpl _$$CourseDetailImplFromJson(Map<String, dynamic> json) =>
     _$CourseDetailImpl(
       takeTime: json['takeTime'] as String,
       distance: json['distance'] as String,
-      courseDetailInfoList: (json['courseDetailInfoList'] as List<dynamic>)
-          .map((e) => CourseDetailInfo.fromJson(e as Map<String, dynamic>))
-          .toList(),
     );
 
 Map<String, dynamic> _$$CourseDetailImplToJson(_$CourseDetailImpl instance) =>
     <String, dynamic>{
       'takeTime': instance.takeTime,
       'distance': instance.distance,
-      'courseDetailInfoList': instance.courseDetailInfoList,
     };
