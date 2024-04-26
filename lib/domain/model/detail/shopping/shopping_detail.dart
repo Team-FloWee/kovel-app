cb8f152247cfe7e21366e054793ac774b696ae00
Yejin.Shin <mingmi_27@naver.com>
2024-04-26 19:21:28 +0900

    feat: Shopping 폴더 Detail 모델 추가

---
+++ lib/domain/model/detail/shopping/shopping_detail.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kovel_app/domain/model/detail/tour_detail.dart';
+
+part 'shopping_detail.freezed.dart';
+part 'shopping_detail.g.dart';
+
+@freezed
+class ShoppingDetail with _$ShoppingDetail {
+  const factory ShoppingDetail({
+    required TourDetail tourDetail,
+    required String saleItem,
+    required String saleItemCost,
+    required String restDay,
+    required String scale,
+    required String creditCard,
+    required String openTime,
+  }) = _ShoppingDetail;
+
+  factory ShoppingDetail.fromJson(Map<String, Object?> json) =>
+      _$ShoppingDetailFromJson(json);
+}
