f3c0825460bf9460011d7246ee916be5d50db401
chojja7188 <shoun7188@naver.com>
2024-04-26 22:48:01 +0900

    feat: 문화시설 모델 수정

--- lib/domain/model/detail/culture_location/culture_location_detail.dart
+++ lib/domain/model/detail/culture_location/culture_location_detail.dart
@@ -8,15 +8,12 @@ part 'culture_location_detail.g.dart';
 @freezed
 class CultureLocationDetail with _$CultureLocationDetail {
   const factory CultureLocationDetail({
-    required TourDetail tourDetail,
     required String useFee,
     required String useTime,
     required String restDay,
     required String parking,
     required String spendTime,
     required String petAllowed,
-
-    required List<CultureLocationDetail> cultureLocationDetailList
   }) = _CultureLocationDetail;
 
   factory CultureLocationDetail.fromJson(Map<String, Object?> json) => _$CultureLocationDetailFromJson(json);
--- lib/domain/model/detail/culture_location/culture_location_detail.g.dart
+++ lib/domain/model/detail/culture_location/culture_location_detail.g.dart
@@ -9,29 +9,21 @@ part of 'culture_location_detail.dart';
 _$CultureLocationDetailImpl _$$CultureLocationDetailImplFromJson(
         Map<String, dynamic> json) =>
     _$CultureLocationDetailImpl(
-      tourDetail:
-          TourDetail.fromJson(json['tourDetail'] as Map<String, dynamic>),
       useFee: json['useFee'] as String,
       useTime: json['useTime'] as String,
       restDay: json['restDay'] as String,
       parking: json['parking'] as String,
       spendTime: json['spendTime'] as String,
       petAllowed: json['petAllowed'] as String,
-      cultureLocationDetailList: (json['cultureLocationDetailList']
-              as List<dynamic>)
-          .map((e) => CultureLocationDetail.fromJson(e as Map<String, dynamic>))
-          .toList(),
     );
 
 Map<String, dynamic> _$$CultureLocationDetailImplToJson(
         _$CultureLocationDetailImpl instance) =>
     <String, dynamic>{
-      'tourDetail': instance.tourDetail,
       'useFee': instance.useFee,
       'useTime': instance.useTime,
       'restDay': instance.restDay,
       'parking': instance.parking,
       'spendTime': instance.spendTime,
       'petAllowed': instance.petAllowed,
-      'cultureLocationDetailList': instance.cultureLocationDetailList,
     };
