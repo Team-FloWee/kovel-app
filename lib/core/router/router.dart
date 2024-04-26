8aa0649418bf61fdaf5d96e52ef2d615202df45d
sseung <sseutta@naver.com>
2024-04-27 02:34:35 +0900

    feat: router 파일 생성
    - HomeViewModel 연결

---
+++ lib/core/router/router.dart
@@ -0,0 +1,21 @@
+// import 'package:go_router/go_router.dart';
+// import 'package:provider/provider.dart';
+//
+// final goRouter = GoRouter(
+//   routes: [
+//   GoRoute(
+//   path: '/',
+//   builder: (context, state) {
+//     return ChangeNotifierProvider(
+//         create: (context) =>
+//             HomeViewModel(
+//               tourInfoRepository: TourInfoRepositoryImpl(
+//                 tourInfoDataSource: TourInfoDataSourceImpl(),
+//               ),
+//               child: const HomeScreen(),
+//             );
+//
+//     ),}
+//
+//   ],
+// )
