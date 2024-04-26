70a8f5b943d6af6fe8c80ed48138089b4decbc4b
chojja7188 <shoun7188@naver.com>
2024-04-27 01:29:50 +0900

    feat: config 폴더 생성 및 기본 작업
    - UiConfig 작성
    - firebase_options.dart 파일 config 폴더에 위치

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:kovel_app/firebase_options.dart';
+import 'package:kovel_app/config/firebase_options.dart';
 import 'package:kovel_app/login_page.dart';
 
 void main() async {
