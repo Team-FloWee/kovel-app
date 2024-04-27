4e52732f71d897779a82dd00adb0e3daf236ab4e
sseung <sseutta@naver.com>
2024-04-27 21:32:07 +0900

    feat: HomeScreen 수정
    -- StatefulWidget -> StatelessWidget

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -2,14 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
