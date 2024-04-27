8d24ac7aa38c29fe97fb30fc5031cee26f60dc78
Han Jung Man <86880025+itbebop@users.noreply.github.com>
2024-04-27 19:21:45 +0900

    style: 오타수정

--- lib/data/data_source/tour_info_data_source_impl.dart
+++ lib/data/data_source/tour_info_data_source_impl.dart
@@ -8,7 +8,7 @@ import 'package:kovel_app/data/dto/tour_dto.dart';
 
 class TourInfoDataSourceImpl implements TourInfoDataSource {
   final _dio = Dio();
-  final _modileOs = 'AND'; // TODO: Core에 Enum으로 만들 예정
+  final _mobileOs = 'AND'; // TODO: Core에 Enum으로 만들 예정
 
   // 지역기반 관광정보조회(areaBasedList1)
   @override
@@ -19,7 +19,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'areaBasedList1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
@@ -34,7 +34,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'locationBasedList1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&mapX=$mapX&mapY=$mapY&radius=$radius&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&mapX=$mapX&mapY=$mapY&radius=$radius&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
@@ -49,7 +49,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'searchFestival1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&eventStartDate=$eventStartDate&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&eventStartDate=$eventStartDate&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
@@ -64,7 +64,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'searchKeyword1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&keyword=$keyword&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&keyword=$keyword&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
@@ -79,7 +79,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'searchStay1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
@@ -95,7 +95,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
 
     Response response;
     response =
-        await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&serviceKey=$key');
+        await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => TourDetailDto.fromJson(e)).toList();
@@ -110,7 +110,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'detailIntro1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => ContentDetailDto.fromJson(e)).toList();
@@ -125,7 +125,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
     const String apiName = 'detailInfo1';
 
     final Response response;
-    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
 
     final List tourInfoList = response.data['response']['body']['items']['item'];
     return tourInfoList.map((e) => ContentDetailInfoDto.fromJson(e)).toList();
