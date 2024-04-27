6eb8fdcc6b1065c72ea79d794a33a9c7fdc2455e
icicle_4 <dwayne.han4@gmail.com>
2024-04-27 16:29:20 +0900

    feat: data_source 수정
    - data_source_interface, data_source_impl 각 변경된 모델에 연결함

--- lib/data/data_source/tour_info_data_source_impl.dart
+++ lib/data/data_source/tour_info_data_source_impl.dart
@@ -1,124 +1,133 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kovel_app/data/data_source/tour_info_data_source.dart';
-import 'package:kovel_app/data/dto/tour_location_dto.dart';
+import 'package:kovel_app/data/dto/content_detail_dto.dart';
+import 'package:kovel_app/data/dto/content_detail_info_dto.dart';
+import 'package:kovel_app/data/dto/tour_detail_dto.dart';
+import 'package:kovel_app/data/dto/tour_dto.dart';
 
 class TourInfoDataSourceImpl implements TourInfoDataSource {
-  // 지역기반 관광정보조회(areaBasedList1)
-  final dio = Dio();
+  final _dio = Dio();
   final _modileOs = 'AND'; // TODO: Core에 Enum으로 만들 예정
 
+  // 지역기반 관광정보조회(areaBasedList1)
   @override
-  //Future<List<TourLocationDto>> getAreaBasedList() async {
-  void getAreaBasedList() async {
+  Future<List<TourDto>> getAreaBasedList() async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL'); // TODO: 전역으로 생성할 방법이 있을까?
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'areaBasedList1';
+    final String baseUrl = dotenv.get('Base_URL'); // TODO: 전역으로 생성할 방법이 있을까?
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'areaBasedList1';
 
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
 
-    print(response.data);
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
   }
 
+  // 위치기반 관광정보조회
   @override
-  //Future<List<TourLocationDto>> getLocationBasedList(String mapX, String mapY, String radius) async {
-  void getLocationBasedList(String mapX, String mapY, String radius) async {
+  Future<List<TourDto>> getLocationBasedList(String mapX, String mapY, String radius) async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'locationBasedList1';
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'locationBasedList1';
 
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&mapX=$mapX&mapY=$mapY&radius=$radius&serviceKey=$key');
-    print(response.data['response']['body']['items']['item']);
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&mapX=$mapX&mapY=$mapY&radius=$radius&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
   }
 
+  // 행사정보조회
   @override
-  //Future<List<TourLocationDto>> getSearchFestival() async {
-  void getSearchFestival(String eventStartDate) async {
+  Future<List<TourDto>> getSearchFestival(String eventStartDate) async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'searchFestival1';
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'searchFestival1';
 
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&eventStartDate=$eventStartDate&serviceKey=$key');
-    print(response.data['response']['body']['items']['item']);
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&eventStartDate=$eventStartDate&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
   }
 
+  // 키워드 검색 조회
   @override
-  //Future<List<TourLocationDto>> getSearchKeyword(String keyword) async {
-  void getSearchKeyword(String keyword) async {
+  Future<List<TourDto>> getSearchKeyword(String keyword) async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'searchKeyword1';
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'searchKeyword1';
 
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&keyword=$keyword&serviceKey=$key');
-    print(response.data['response']['body']['items']['item']);
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&keyword=$keyword&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
   }
 
+  // 숙박정보조회
   @override
-  // Future<List<TourLocationDto>> getSearchStay() async {
-  void getSearchStay() async {
+  Future<List<TourDto>> getSearchStay() async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'searchStay1';
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'searchStay1';
 
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
-    print(response.data['response']['body']['items']['item']);
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDto.fromJson(e)).toList();
   }
 
+  // 공통정보조회
   @override
-  Future<dynamic> getDetailIntro(String contentId, String contentTypeId) async {
+  Future<List<TourDetailDto>> getDetailCommon(String contentId) async {
     await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'detailIntro1';
-
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
-    print(response.data);
-  }
-
-  @override
-  //Future<List> getDetailInfo() async {
-  void getDetailInfo(String contentId, String contentTypeId) async {
-    await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'detailInfo1';
-
-    Response response;
-    response = await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
-    print(response.data);
-  }
-
-  @override
-  Future getDetailCommon(String contentId, String contentTypeId) async {
-    await dotenv.load();
-    String baseUrl = dotenv.get('Base_URL');
-    String key = dotenv.get('TOUR_API_KEY');
-    String apiName = 'detailCommon1';
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'detailCommon1';
 
     Response response;
     response =
-        await dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&serviceKey=$key');
-    print(response.data);
-  }
-}
+        await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&serviceKey=$key');
 
-main() {
-  //TourInfoDataSourceImpl().getAreaBasedList();
-  //TourInfoDataSourceImpl().getLocationBasedList('127.7130161606', '37.9167408445', '500');
-  // TourInfoDataSourceImpl().getSearchFestival('20240401');
-  // TourInfoDataSourceImpl().getSearchStay();
-  // TourInfoDataSourceImpl().getDetailCommon('132248');
-  // TourInfoDataSourceImpl().getDetailIntro('3111770', '15');
-  TourInfoDataSourceImpl().getDetailInfo('2384832', '25');
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => TourDetailDto.fromJson(e)).toList();
+  }
+
+  // 소개정보조회
+  @override
+  Future<List<ContentDetailDto>> getDetailIntro(String contentId, String contentTypeId) async {
+    await dotenv.load();
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'detailIntro1';
+
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => ContentDetailDto.fromJson(e)).toList();
+  }
+
+  // 반복정보조회
+  @override
+  Future<List<ContentDetailInfoDto>> getDetailInfo(String contentId, String contentTypeId) async {
+    await dotenv.load();
+    final String baseUrl = dotenv.get('Base_URL');
+    final String key = dotenv.get('TOUR_API_KEY');
+    const String apiName = 'detailInfo1';
+
+    final Response response;
+    response = await _dio.get('$baseUrl/$apiName?MobileOS=$_modileOs&MobileApp=MobileApp&_type=json&contentId=$contentId&contentTypeId=$contentTypeId&serviceKey=$key');
+
+    final List tourInfoList = response.data['response']['body']['items']['item'];
+    return tourInfoList.map((e) => ContentDetailInfoDto.fromJson(e)).toList();
+  }
 }
