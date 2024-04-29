a93f6f8a0dae510014188ce975f32f172e7cc7b5
icicle_4 <dwayne.han4@gmail.com>
2024-04-30 00:50:33 +0900

    feat: bottom_navi_bar 생성

---
+++ lib/presentation/components/bottom_navi_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:kovel_app/config/ui_config.dart';
+
+class BottomNaviBar extends StatelessWidget {
+  const BottomNaviBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(top: BorderSide(color: UiConfig.primaryColor, width: 0.5)), // 라인효과
+      ),
+      child: BottomNavigationBar(
+        elevation: 0,
+        onTap: (value) {
+          switch (value) {
+            case 0:
+            // context.go('/');
+            case 1:
+            // context.go('/bookmarks');
+          }
+        },
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: UiConfig.primaryColor,
+            ),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_border,
+                color: UiConfig.black.shade700,
+              ),
+              label: '보관함'),
+          const BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '마이페이지'),
+        ],
+        selectedLabelStyle: const TextStyle(
+          fontWeight: UiConfig.semiBoldFont,
+        ),
+        selectedItemColor: UiConfig.primaryColor,
+        unselectedLabelStyle: const TextStyle(fontWeight: UiConfig.semiBoldFont),
+      ),
+    );
+  }
+}
