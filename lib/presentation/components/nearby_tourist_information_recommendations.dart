74d2bcd2214fdeb70f4f61fe596840cacce5d26b
icicle_4 <dwayne.han4@gmail.com>
2024-04-30 11:21:53 +0900

    style: radius 8 추가

--- lib/presentation/components/nearby_tourist_information_recommendations.dart
+++ lib/presentation/components/nearby_tourist_information_recommendations.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 class NearbyTouristInformation extends StatelessWidget {
   final String url;
 
-  const NearbyTouristInformation( {super.key, required this.url});
+  const NearbyTouristInformation({super.key, required this.url});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Image.network(
-        url,
-        fit: BoxFit.cover,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.network(
+          url,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
