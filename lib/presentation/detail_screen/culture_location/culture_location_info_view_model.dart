8447be70a4f70109d583173666f525128444952c
Yejin.Shin <mingmi_27@naver.com>
2024-04-30 21:14:06 +0900

    feat: 상세정보에서 데이터가 ''면 IconTextRow()가 안보이는 기능 추가.

--- lib/presentation/detail_screen/culture_location/culture_location_info_view_model.dart
+++ lib/presentation/detail_screen/culture_location/culture_location_info_view_model.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:kovel_app/domain/model/detail/culture_location/culture_location_detail.dart';
 import 'package:kovel_app/domain/model/detail/culture_location/culture_location_detail_info.dart';
 import 'package:kovel_app/domain/model/detail/tour_detail.dart';
 import 'package:kovel_app/domain/repository/tour_info_repository.dart';
+import 'package:kovel_app/presentation/components/icon_text_row.dart';
 
 class CultureLocationInfoViewModel with ChangeNotifier {
   final TourInfoRepository _tourInfoRepository;
@@ -12,9 +15,21 @@ class CultureLocationInfoViewModel with ChangeNotifier {
   }) : _tourInfoRepository = tourInfoRepository;
 
   bool _isLoading = true;
+  List<Widget> widgets = [];
   List<TourDetail> _tourDetailData = [];
   List<CultureLocationDetail> _cultureLocationDetailData = [];
   List<CultureLocationDetailInfo> _cultureLocationInfoData = [];
+
+  // Map<String, dynamic> mockdata = CultureLocationDetail(
+  //     contentId: 129854,
+  //     contentTypeId: 14,
+  //     infoCenter: "02-720-1020",
+  //     useFee: " 전시마다 상이",
+  //     useTime: "",
+  //     restDay: "월요일",
+  //     parking: "",
+  //     spendTime: "",
+  //     petAllowed: "불가") as Map<String, dynamic>;
 
   bool get isLoading => _isLoading;
   List<TourDetail> get tourDetailData => _tourDetailData;
@@ -26,6 +41,7 @@ class CultureLocationInfoViewModel with ChangeNotifier {
   Future<void> getCultureLocationData(int id, int contentTypeId) async {
     _isLoading = true;
     notifyListeners();
+
     _tourDetailData = await _tourInfoRepository.getDetailCommon(id: id);
     _cultureLocationDetailData = await _tourInfoRepository
         .getCultureLocationDetail(id: id, contentTypeId: contentTypeId);
@@ -33,6 +49,26 @@ class CultureLocationInfoViewModel with ChangeNotifier {
         .getCultureLocationDetailInfo(id: id, contentTypeId: contentTypeId);
 
     _isLoading = false;
+    showDetailData();
     notifyListeners();
+  }
+
+  void showDetailData() async {
+    Map<String, dynamic> detailData =
+        jsonDecode(jsonEncode(_cultureLocationDetailData.first));
+
+    detailData.forEach((key, value) {
+      if (value != null &&
+          value != '' &&
+          key != 'contentId' &&
+          key != 'contentTypeId') {
+        widgets.add(
+          IconTextRow(
+            icon: Icons.phone,
+            text: value.toString(),
+          ),
+        );
+      }
+    });
   }
 }
