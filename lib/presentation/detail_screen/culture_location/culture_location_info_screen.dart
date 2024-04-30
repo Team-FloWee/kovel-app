0d3d5ee71eda217de76ed2a499402fdcc329c656
Yejin.Shin <mingmi_27@naver.com>
2024-04-30 15:03:44 +0900

    feat: add CultureLocation Screen & Viewmodel

--- lib/presentation/detail_screen/culture_location/culture_location_info_screen.dart
+++ lib/presentation/detail_screen/culture_location/culture_location_info_screen.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:kovel_app/config/ui_config.dart';
+import 'package:kovel_app/presentation/components/common_app_bar.dart';
+import 'package:kovel_app/presentation/components/common_text.dart';
+import 'package:kovel_app/presentation/components/icon_text_row.dart';
+import 'package:kovel_app/presentation/components/info_text.dart';
+import 'package:kovel_app/presentation/detail_screen/culture_location/culture_location_info_view_model.dart';
+import 'package:provider/provider.dart';
 
-class CultureLocationInfoScreen extends StatelessWidget {
-  const CultureLocationInfoScreen({super.key});
+class CultureLocationInfoScreen extends StatefulWidget {
+  final int id;
+  final int contentTypeId;
+  const CultureLocationInfoScreen(
+      {super.key, required this.id, required this.contentTypeId});
+
+  @override
+  State<CultureLocationInfoScreen> createState() =>
+      _CultureLocationInfoScreenState();
+}
+
+class _CultureLocationInfoScreenState extends State<CultureLocationInfoScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => context
+          .read<CultureLocationInfoViewModel>()
+          .getCultureLocationData(widget.id, widget.contentTypeId),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<CultureLocationInfoViewModel>();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('아직안댐'),
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back_ios),
-        ),
-      ),
+      appBar: CommonAppBar(title: viewModel.tourDetailData.first.title),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -24,136 +44,77 @@ class CultureLocationInfoScreen extends StatelessWidget {
                   "https://images.unsplash.com/photo-1472791108553-c9405341e398?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"),
             ),
             Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    //이후 컴포넌트로 교체
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  CommonText(
+                      badgeTitle: '문화시설',
+                      title: viewModel.tourDetailData.first.title,
+                      tel: viewModel.tourDetailData.first.tel,
+                      address: viewModel.tourDetailData.first.address1),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 18, bottom: 16),
+                    child: Divider(
+                        thickness: 1,
+                        height: 1,
+                        color: UiConfig.black.shade500),
+                  ),
+                  IconTextRow(
+                      icon: Icons.monetization_on,
+                      text: viewModel.cultureLocationDetailData.first.useFee),
+                  IconTextRow(
+                      icon: Icons.access_time_filled,
+                      text: viewModel.cultureLocationDetailData.first.useTime),
+                  IconTextRow(
+                      icon: Icons.door_front_door,
+                      text: viewModel.cultureLocationDetailData.first.restDay),
+                  IconTextRow(
+                      icon: Icons.local_parking,
+                      text: viewModel.cultureLocationDetailData.first.parking),
+                  IconTextRow(
+                      icon: Icons.timelapse,
+                      text:
+                          viewModel.cultureLocationDetailData.first.spendTime),
+                  IconTextRow(
+                      icon: Icons.pets,
+                      text:
+                          viewModel.cultureLocationDetailData.first.petAllowed),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 18, bottom: 16),
+                    child: Divider(
+                        thickness: 1,
+                        height: 1,
+                        color: UiConfig.black.shade500),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 16, top: 16, right: 16, bottom: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: UiConfig.black.shade500,
+                    ),
+                    child: Column(
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Container(
-                                color: Color(0xFF00D1FF),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 4, horizontal: 10),
-                                  child: Text('음식점', style: UiConfig.h4Style),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            Text(
-                              '오리 주물럭 집',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  letterSpacing: -0.3),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.phone,
-                                  size: 16,
-                                ),
-                                SizedBox(
-                                  width: 2,
-                                ),
-                                Text(
-                                  '010-1234-5678',
-                                  style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: -0.3),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.location_on_rounded,
-                                  size: 16,
-                                ),
-                                SizedBox(
-                                  width: 2,
-                                ),
-                                Text(
-                                  '서울 관악구 관악시 11로 12길',
-                                  style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: -0.3),
-                                ),
-                              ],
-                            ),
-                          ],
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: 16, top: 16, right: 16, bottom: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: UiConfig.black.shade500,
+                          ),
+                          child: Column(
+                            children: viewModel.cultureLocationInfoData
+                                .map((e) => InfoText(
+                                    title: e.infoName, contents: e.infoText))
+                                .toList(),
+                          ),
                         ),
                       ],
                     ),
-                    //컴포넌트로 교체 끝
-                    Padding(
-                      padding: const EdgeInsets.only(top: 18, bottom: 16),
-                      child: Divider(
-                          thickness: 1,
-                          height: 1,
-                          color: UiConfig.black.shade500),
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.monetization_on),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Text(
-                          '무료',
-                          style: UiConfig.bodyStyle
-                              .copyWith(fontWeight: UiConfig.semiBoldFont),
-                        ),
-                        //Icon(Icons.access_time_filled),
-                        //Icon(Icons.door_front_door),
-                        //Icon(Icons.local_parking),
-                        //Icon(Icons.timelapse),
-                        //Icon(Icons.pets),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 18, bottom: 16),
-                      child: Divider(
-                          thickness: 1,
-                          height: 1,
-                          color: UiConfig.black.shade500),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(
-                          left: 16, top: 16, right: 16, bottom: 8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: UiConfig.black.shade500,
-                      ),
-                      child: Row(
-                        children: [
-                          Row(
-                            children: [
-                              Text('인포텍스트입니다'),
-                              Text('216Km'),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
