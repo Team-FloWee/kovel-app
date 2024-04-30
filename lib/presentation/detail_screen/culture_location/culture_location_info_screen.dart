1fdbd2fc25970a0952be914aed1f8ab947bae74d
Yejin.Shin <mingmi_27@naver.com>
2024-04-30 21:14:06 +0900

    feat: 상세정보에서 데이터가 ''면 IconTextRow()가 안보이는 기능 추가.

--- lib/presentation/detail_screen/culture_location/culture_location_info_screen.dart
+++ lib/presentation/detail_screen/culture_location/culture_location_info_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kovel_app/config/ui_config.dart';
 import 'package:kovel_app/presentation/components/common_app_bar.dart';
 import 'package:kovel_app/presentation/components/common_text.dart';
-import 'package:kovel_app/presentation/components/icon_text_row.dart';
 import 'package:kovel_app/presentation/components/info_text.dart';
 import 'package:kovel_app/presentation/detail_screen/culture_location/culture_location_info_view_model.dart';
 import 'package:provider/provider.dart';
@@ -10,8 +9,23 @@ import 'package:provider/provider.dart';
 class CultureLocationInfoScreen extends StatefulWidget {
   final int id;
   final int contentTypeId;
-  const CultureLocationInfoScreen(
-      {super.key, required this.id, required this.contentTypeId});
+  final String title;
+  final Map<String, dynamic> mockdata = {
+    "contentId": 129854,
+    "contentTypeId": 14,
+    "infoCenter": "02-720-1020",
+    "useFee": " 전시마다 상이",
+    "useTime": "",
+    "restDay": "월요일",
+    "parking": "",
+    "spendTime": "",
+    "petAllowed": "불가"
+  };
+  CultureLocationInfoScreen(
+      {super.key,
+      required this.id,
+      required this.contentTypeId,
+      required this.title});
 
   @override
   State<CultureLocationInfoScreen> createState() =>
@@ -22,79 +36,60 @@ class _CultureLocationInfoScreenState extends State<CultureLocationInfoScreen> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-      () => context
-          .read<CultureLocationInfoViewModel>()
-          .getCultureLocationData(widget.id, widget.contentTypeId),
-    );
+    Future.microtask(() => {
+          context
+              .read<CultureLocationInfoViewModel>()
+              .getCultureLocationData(widget.id, widget.contentTypeId),
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<CultureLocationInfoViewModel>();
     return Scaffold(
-      appBar: CommonAppBar(title: viewModel.tourDetailData.first.title),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            AspectRatio(
-              aspectRatio: 1 / 1,
-              child: Image.network(
-                  fit: BoxFit.cover,
-                  "https://images.unsplash.com/photo-1472791108553-c9405341e398?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"),
-            ),
-            Padding(
-              padding: EdgeInsets.all(16),
+      appBar: CommonAppBar(title: widget.title),
+      body: viewModel.isLoading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
               child: Column(
                 children: [
-                  CommonText(
-                      badgeTitle: '문화시설',
-                      title: viewModel.tourDetailData.first.title,
-                      tel: viewModel.tourDetailData.first.tel,
-                      address: viewModel.tourDetailData.first.address1),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 18, bottom: 16),
-                    child: Divider(
-                        thickness: 1,
-                        height: 1,
-                        color: UiConfig.black.shade500),
+                  AspectRatio(
+                    aspectRatio: 1 / 1,
+                    child: Image.network(
+                        fit: BoxFit.cover,
+                        "https://images.unsplash.com/photo-1472791108553-c9405341e398?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D"),
                   ),
-                  IconTextRow(
-                      icon: Icons.monetization_on,
-                      text: viewModel.cultureLocationDetailData.first.useFee),
-                  IconTextRow(
-                      icon: Icons.access_time_filled,
-                      text: viewModel.cultureLocationDetailData.first.useTime),
-                  IconTextRow(
-                      icon: Icons.door_front_door,
-                      text: viewModel.cultureLocationDetailData.first.restDay),
-                  IconTextRow(
-                      icon: Icons.local_parking,
-                      text: viewModel.cultureLocationDetailData.first.parking),
-                  IconTextRow(
-                      icon: Icons.timelapse,
-                      text:
-                          viewModel.cultureLocationDetailData.first.spendTime),
-                  IconTextRow(
-                      icon: Icons.pets,
-                      text:
-                          viewModel.cultureLocationDetailData.first.petAllowed),
                   Padding(
-                    padding: const EdgeInsets.only(top: 18, bottom: 16),
-                    child: Divider(
-                        thickness: 1,
-                        height: 1,
-                        color: UiConfig.black.shade500),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(
-                        left: 16, top: 16, right: 16, bottom: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: UiConfig.black.shade500,
-                    ),
+                    padding: EdgeInsets.all(16),
                     child: Column(
                       children: [
+                        Row(
+                          children: [
+                            CommonText(
+                              badgeTitle: 'badgeTitle',
+                              title: viewModel.tourDetailData.first.title,
+                              tel: viewModel.tourDetailData.first.tel,
+                              address: viewModel.tourDetailData.first.address1,
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 18, bottom: 16),
+                          child: Divider(
+                              thickness: 1,
+                              height: 1,
+                              color: UiConfig.black.shade500),
+                        ),
+                        ...viewModel.widgets.map((e) => e),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 18, bottom: 16),
+                          child: Divider(
+                              thickness: 1,
+                              height: 1,
+                              color: UiConfig.black.shade500),
+                        ),
                         Container(
                           padding: EdgeInsets.only(
                               left: 16, top: 16, right: 16, bottom: 8),
@@ -103,10 +98,25 @@ class _CultureLocationInfoScreenState extends State<CultureLocationInfoScreen> {
                             color: UiConfig.black.shade500,
                           ),
                           child: Column(
-                            children: viewModel.cultureLocationInfoData
-                                .map((e) => InfoText(
-                                    title: e.infoName, contents: e.infoText))
-                                .toList(),
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(
+                                    left: 16, top: 16, right: 16, bottom: 8),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: UiConfig.black.shade500,
+                                ),
+                                child: Column(
+                                  children: viewModel.cultureLocationInfoData
+                                      .map(
+                                        (e) => InfoText(
+                                            title: e.infoName,
+                                            contents: e.infoText),
+                                      )
+                                      .toList(),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
@@ -115,9 +125,6 @@ class _CultureLocationInfoScreenState extends State<CultureLocationInfoScreen> {
                 ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
