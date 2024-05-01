cbc5db81c87e364bc1650c222d045b413513d96a
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-05-01 22:44:23 +0900

    refactor: edit TourInfoDataSource parameter on getSearchFestival()
    - TourInfoDataSource & TourInfoDataSourceImpl 의 getSearchFestival() 메서드의 파라미터 수정
    (required String eventEndDate 추가)

--- lib/data/data_source/tour_info_data_source_impl.dart
+++ lib/data/data_source/tour_info_data_source_impl.dart
@@ -49,7 +49,7 @@ class TourInfoDataSourceImpl implements TourInfoDataSource {
 
   // 행사정보조회
   @override
-  Future<List<TourDto>> getSearchFestival({required String eventStartDate, int pageNo = 1}) async {
+  Future<List<TourDto>> getSearchFestival({required String eventStartDate, required String eventEndDate, int pageNo = 1}) async {
     const String apiName = 'searchFestival1';
     final Response response;
     response = await _dio.get('$baseUrl/$apiName?pageNo=$pageNo&MobileOS=$_mobileOs&MobileApp=MobileApp&_type=json&arrange=Q&eventStartDate=$eventStartDate&serviceKey=$key');
