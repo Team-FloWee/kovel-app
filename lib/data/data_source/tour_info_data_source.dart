cab795e7a366ccf91568aede3e68161a68afc3c5
Yejin.Shin <mingmi_27@naver.com>
2024-05-02 10:01:44 +0900

    feat: getAreaBasedList 파라미터 3개 추가

--- lib/data/data_source/tour_info_data_source.dart
+++ lib/data/data_source/tour_info_data_source.dart
@@ -4,13 +4,23 @@ import 'package:kovel_app/data/dto/tour_detail_dto.dart';
 import 'package:kovel_app/data/dto/tour_dto.dart';
 
 abstract interface class TourInfoDataSource {
-  Future<List<TourDto>> getAreaBasedList({int pageNo = 1});
-  Future<List<TourDto>> getLocationBasedList({required String mapX, required String mapY, required String radius, int pageNo = 1});
-  Future<List<TourDto>> getSearchKeyword({required String keyword, int pageNo = 1});
-  Future<List<TourDto>> getSearchFestival({required String eventStartDate, int pageNo = 1});
+  Future<List<TourDto>> getAreaBasedList(
+      {int pageNo = 1, int? contentTypeId, String? areaCode, String? cat2});
+  Future<List<TourDto>> getLocationBasedList(
+      {required String mapX,
+      required String mapY,
+      required String radius,
+      int pageNo = 1});
+  Future<List<TourDto>> getSearchKeyword(
+      {required String keyword, int pageNo = 1});
+  Future<List<TourDto>> getSearchFestival(
+      {required String eventStartDate, int pageNo = 1});
   Future<List<TourDto>> getSearchStay({int? pageNo = 1});
 
-  Future<List<TourDetailDto>> getDetailCommon({required int id, int pageNo = 1});
-  Future<List<ContentDetailDto>> getDetailIntro({required int id, required int contentTypeId, int pageNo = 1});
-  Future<List<ContentDetailInfoDto>> getDetailInfo({required int id, required int contentTypeId, int pageNo = 1});
+  Future<List<TourDetailDto>> getDetailCommon(
+      {required int id, int pageNo = 1});
+  Future<List<ContentDetailDto>> getDetailIntro(
+      {required int id, required int contentTypeId, int pageNo = 1});
+  Future<List<ContentDetailInfoDto>> getDetailInfo(
+      {required int id, required int contentTypeId, int pageNo = 1});
 }
