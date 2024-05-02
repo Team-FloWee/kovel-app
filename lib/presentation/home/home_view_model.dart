37dd7a4e5df6a428000b9716e8f45462da842988
itbebop <it.bebop@gmail.com>
2024-05-02 17:58:56 +0900

    feat: 진행 중인 축제 모음 추가
    - 진행중인지 여부 추가 조건 넣어야함

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
 import 'package:kovel_app/data/data_source/address_info_data_source_impl.dart';
+import 'package:kovel_app/data/data_source/tour_info_data_source.dart';
 import 'package:kovel_app/data/data_source/tour_info_data_source_impl.dart';
 import 'package:kovel_app/data/repository_impl/address_info_repository_impl.dart';
 import 'package:kovel_app/data/repository_impl/tour_info_repository_impl.dart';
@@ -12,23 +14,26 @@ class HomeViewModel with ChangeNotifier {
   HomeViewModel({
     required TourInfoRepositoryImpl tourInfoRepository,
   }) : _tourInfoRepository = tourInfoRepository;
+  bool isLoading = false;
   double? _longitude;
   double? _latitude;
   String selectedLocation = '현재 위치';
   Position? currentPosition;
-  List<String> locationList = ['현재 위치'];
-  void onFetch() {
-    // 위치 초기값 설정
-    // if (selectedLocation == '') {
-    //   selectedLocation = locationList.first;
-    // } else {
-    //   selectedLocation = locationList.last;
-    // }
+  List<String> locationList = ['현재 위치']; // TODO:초기값은 firebase연결 후에 이전 연결주소
+  List<Tour> onGoingTourList = [];
+  List<Tour> locationBasedList = []; // TODO: 초기값 firebase연결 후에 이전 받아온 관광정보
 
-    print('==============fetch됨');
+  void onFetch() {
+    isLoading = true;
     notifyListeners();
+    fetchOnGoingFestival();
+    print('festival 패치됨');
+    notifyListeners();
+    isLoading = false;
+    print('==============fetch됨');
   }
 
+  // 주소 새로고침
   void refreshPosition(String radiusData) async {
     List<String> radiusList = radiusData.split('km');
     String radius = radiusList[0];
@@ -36,6 +41,7 @@ class HomeViewModel with ChangeNotifier {
     bool serviceEnabled;
     LocationPermission permission;
 
+    /* 위도 경도 가져오기 시작 */
     // GPS 켜져있는지
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -49,13 +55,11 @@ class HomeViewModel with ChangeNotifier {
         return Future.error('Location permissions are denied');
       }
     }
-
     // 두번 이상 거부하면 계속 거부한 것으로 보고 에러냄
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
       return Future.error('Location permissions are permanently denied, we cannot request permissions.');
     }
-
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
     final Position currentPosition = await Geolocator.getCurrentPosition();
@@ -64,30 +68,22 @@ class HomeViewModel with ChangeNotifier {
     if (_longitude != null && _longitude! < 0) {
       _longitude = currentPosition.longitude * -1;
     }
-    // print('_longitude: $_longitude');
-    // print('_latitude: $_latitude');
+    /* 위도 경도 가져오기 끝 */
+
+    // 위도,경도로 주소 가져오기
     fetchAddressData(longitude: _longitude!.toString(), latitude: _latitude!.toString());
-    // List tourData = await _tourInfoRepository.getLocationBasedList(mapX: _longitude.toString(), mapY: _latitude.toString(), radius: radius);
-    // List tourData = await _tourInfoRepository.getLocationBasedList(mapX: '127.1216721795'.toString(), mapY: '36.4529297875'.toString(), radius: radius);
-    //print(tourData[0]);
 
-    //fetchAddressData('127.1216721795'.toString(), '36.4529297875'.toString()); // 안나옴
-    //fetchAddressData(lon: '127.423084873712'.toString(), lat: '37.0789561558879'.toString()); // 나옴
-
-    //fetchOnGoingFestival();
-    locationList.forEach(print);
     notifyListeners();
-    print('================보냄1');
+    print('================notifyListeners1');
   }
 
+  // 위도,경도로 주소 가져오기
   void fetchAddressData({required String longitude, required String latitude}) async {
     print('===fetchAddressData시작');
     print('latitude: $latitude');
     print('longitude: $longitude');
-    // 잘 나오는지 확인!
+    // 주소 받아옴
     final dataList = await AddressInfoRepositoryImpl(addressInfoDataSource: AddressInfoDataSourceImpl()).getAddress(longitude: longitude, latitude: latitude);
-    // print('#####dataList : $dataList');
-    // print('#####dataList.first : ${dataList.first}');
 
     // 구/신주소 중 데이터가 있는 것을 locationList에 넣음
     if (dataList.first.roadAddress.addressName != '' && dataList.first.oldAddress.addressName != '') {
@@ -95,14 +91,35 @@ class HomeViewModel with ChangeNotifier {
     } else if (dataList.first.oldAddress.addressName != '') {
       locationList.insert(0, dataList.first.oldAddress.addressName);
     }
-
-    locationList.length > 1 ? locationList.removeWhere((element) => element == '현재 위치') : print('한개만 있음');
-    !locationList.contains(selectedLocation) ? selectedLocation = locationList.first : print('같음');
+    // 위치 목록이 2이상 되면(주소 받아오면) 초기값 삭제
+    locationList.length > 1
+        ? locationList.removeWhere((element) => element == '현재 위치')
+        : !locationList.contains(selectedLocation)
+            ? selectedLocation = locationList.first
+            : print('같음');
     notifyListeners(); // TODO: await 때문에 notifyListeners()실행 후 주소가져옴. 해결방법은?
-    print('================보냄2');
+    print('================notifyListeners2');
   }
 
+  // 내 주변 관광정보
+  void fetchLocationBasedList(String latitude, String longitude, String radius) async {
+    locationBasedList = await TourInfoRepositoryImpl(tourInfoDataSource: TourInfoDataSourceImpl(dio: Dio())).getLocationBasedList(mapX: longitude, mapY: latitude, radius: radius);
+  }
+
+  // 진행중인 축제 모음
   void fetchOnGoingFestival() async {
-    final List<Tour> tourList = await TourInfoRepositoryImpl(tourInfoDataSource: TourInfoDataSourceImpl(dio: Dio())).getSearchFestival(eventStartDate: '20240101', eventEndDate: '20240502');
+    isLoading = true;
+    final today = DateTime.now();
+    final tomorrow = today.add(const Duration(days: 1));
+    print(DateFormat('yyyyMMdd').format(tomorrow));
+    notifyListeners();
+    onGoingTourList =
+        await TourInfoRepositoryImpl(tourInfoDataSource: TourInfoDataSourceImpl(dio: Dio())).getSearchFestival(eventStartDate: '20240101', eventEndDate: DateFormat('yyyyMMdd').format(today));
+    isLoading = false;
+    notifyListeners();
+    //print('onGoingTourList: ${onGoingTourList[0].imagePath}');
+    for (var element in onGoingTourList) {
+      print(element);
+    }
   }
 }
