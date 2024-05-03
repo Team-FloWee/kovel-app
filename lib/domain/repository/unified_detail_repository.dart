ad271bddb3ad858e0e0eabb3180f87df7521d489
chojja7188 <shoun7188@naver.com>
2024-05-03 09:59:19 +0900

    chore: merge conflict resolve 덜 된 부분 수정

--- lib/domain/repository/unified_detail_repository.dart
+++ lib/domain/repository/unified_detail_repository.dart
@@ -29,6 +29,7 @@ abstract interface class UnifiedDetailRepository {
   Future<List<Tour>> getSearchFestival({
     int pageNo = 1,
     required String eventStartDate,
+    required String eventEndDate,
   });
 
   // 숙박 정보 조회 (search stay)
