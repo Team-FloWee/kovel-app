a5d7220d7dd21e902283b2fa9214ad5ae1e0b0dd
sseung <sseutta@naver.com>
2024-05-03 12:15:17 +0900

    refactor: course_list_screen.dart
    -- category_list.dart 컴포넌트 빼고 작성

--- lib/presentation/course_list/course_list_screen.dart
+++ lib/presentation/course_list/course_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kovel_app/config/ui_config.dart';
+import 'package:kovel_app/presentation/components/category_item.dart';
 import 'package:kovel_app/presentation/components/category_list.dart';
 import 'package:kovel_app/presentation/components/common_app_bar.dart';
 import 'package:kovel_app/presentation/course_list/course_list_view_model.dart';
@@ -25,64 +26,82 @@ class _CourseListScreenState extends State<CourseListScreen> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<CourseListViewModel>();
-    return Scaffold(
-      appBar: CommonAppBar(
-        title: '전체 여행코스',
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Text(
-                  '추천코스',
-                  style: UiConfig.h3Style.copyWith(
-                    fontWeight: UiConfig.semiBoldFont,
-                    color: UiConfig.black.shade900,
-                  ),
+    return viewModel.isLoading == true
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: CommonAppBar(
+              title: '전체 여행코스',
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        '추천코스',
+                        style: UiConfig.h3Style.copyWith(
+                          fontWeight: UiConfig.semiBoldFont,
+                          color: UiConfig.black.shade900,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    // CategoryList(textdata: [
+                    //   '전체',
+                    //   'C0112',
+                    //   'C0113',
+                    //   'C0114',
+                    //   'C0115',
+                    //   'C0116',
+                    //   'C0117',
+                    // ]),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Row(
+                          // children: widget.textdata
+                          //     .map((e) => GestureDetector(onTap: onTapCourseData(e) ,child: CategoryItem(e,)))
+                          //     .toList(),
+                          children: viewModel.textdata
+                              .map((e) => GestureDetector(
+                                  onTap: () {
+                                    viewModel.onTapCourseData(e);
+                                  },
+                                  child: CategoryItem(e)))
+                              .toList(),
+                        ), //This expression has a type of
+                        // 'void' so its value can't be used. (Documentation)
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Column(
+                      children: viewModel.TourDetailData.map((e) => Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: CourseRecommendation(
+                              url: e.imagePath,
+                              course: '음식점',
+                              title: e.title,
+                              content: e.overview,
+                            ),
+                          )).toList(),
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 16,
-              ),
-              CategoryList(textdata: [
-                '전체',
-                '가족코스',
-                '나홀로코스',
-                '힐링코스',
-                '도보코스',
-                '캠핑코스',
-                '맛코스',
-              ]),
-              SizedBox(
-                height: 16,
-              ),
-              Column(
-                children:
-                  viewModel.TourDetailData.map((e) => Padding(
-                    padding: const EdgeInsets.only(bottom: 16.0),
-                    child: CourseRecommendation(
-                      url:
-                      e.imagePath,
-                      course: '음식점',
-                      title: e.title,
-                      content:
-                      e.overview,
-                    ),
-                  )).toList(),
-
-
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
