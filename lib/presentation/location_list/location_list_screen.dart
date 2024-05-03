089d549f8c7d60744f072cc015406d7f83769765
jungeun272 <hanjungeun272@gmail.com>
2024-05-03 12:53:01 +0900

    feat: statefulWidget 두개로 나눔

--- lib/presentation/location_list/location_list_screen.dart
+++ lib/presentation/location_list/location_list_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:kovel_app/presentation/components/category_list.dart';
 import 'package:kovel_app/presentation/components/common_app_bar.dart';
@@ -14,7 +15,9 @@ import 'package:provider/provider.dart';
 import '../../core/enums/content_type.dart';
 
 class LocationListScreen extends StatefulWidget {
-  const LocationListScreen({super.key});
+  final String areaCode;
+
+  const LocationListScreen({super.key, required this.areaCode});
 
   @override
   State<LocationListScreen> createState() => _LocationListScreenState();
@@ -24,8 +27,8 @@ class _LocationListScreenState extends State<LocationListScreen> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-        () => context.read<LocationListViewModel>().getCourseData()); //세트
+    Future.microtask(() =>
+        context.read<LocationListViewModel>().getData(widget.areaCode)); //세트
   }
 
   @override
@@ -42,7 +45,7 @@ class _LocationListScreenState extends State<LocationListScreen> {
                   Padding(
                     padding: const EdgeInsets.only(left: 16.0, right: 16.0),
                     child: ContentTitle(
-                      title: '추천코스',
+                      title: '추천 코스',
                       withMore: true,
                     ),
                   ),
@@ -74,61 +77,89 @@ class _LocationListScreenState extends State<LocationListScreen> {
                   SizedBox(
                     height: 24,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                    child: ContentTitle(
-                      title: '테마별 장소',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: CategoryList(textdata:
-                      ContentType.values
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-                    child: Column(
-                      children: viewModel.tourDetail
-                          .map(
-                            (e) => Padding(
-                              padding: const EdgeInsets.only(bottom: 16.0),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  viewModel.isLoading == true
-                                      ? Center(
-                                          child: CircularProgressIndicator())
-                                      : FavoriteImage(
-                                          imagePath: e.imagePath,
-                                          imageSize: 145,
-                                        ),
-                                  SizedBox(width: 8),
-                                  CommonText(
-                                    badgeTitle: '음식점',
-                                    title: e.title,
-                                    tel: e.tel,
-                                    address: e.address1,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
+                  LocationCommonData(areaCode: widget.areaCode)
                 ],
               ),
             ),
           );
   }
 }
+
+class LocationCommonData extends StatefulWidget {
+  final String areaCode;
+
+  const LocationCommonData(
+      {super.key, required this.areaCode,});
+
+  @override
+  State<LocationCommonData> createState() => _LocationCommonDataState();
+}
+
+class _LocationCommonDataState extends State<LocationCommonData> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<LocationListViewModel>();
+
+    return SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: ContentTitle(
+                  title: '테마별 장소',
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 16),
+                  CategoryList(textdata: ContentType.values),
+                  SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      children: viewModel.tourDetail
+                          .map((e) => Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            viewModel.isLoading
+                                ? Center(
+                                child: CircularProgressIndicator())
+                                : FavoriteImage(
+                              imagePath: e.imagePath,
+                              imageSize: 145,
+                            ),
+                            SizedBox(width: 8),
+                            CommonText(
+                              badgeTitle: '음식점',
+                              title: e.title,
+                              tel: e.tel,
+                              address: e.address1,
+                            ),
+                          ],
+                        ),
+                      ))
+                          .toList(),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ],
+          ),
+        );
+
+  }
+}
+
+//해야할 일 스테이트풀 위젯 두개로 나누기 하나는 코스가 포함된 위젯, 두번쨰는 commondetail 이 포함된 위젯
+//initstate할때 세가지 메서드중에서 getData메서드를 사용하여 코스추천메서드와 테마별여행메서드를 동시에 호출
+// 뱃지를 누를때마다 해달 섹션만 로딩 할 수 있도록 코스추천메서드와 테마별여행메서드 각자 사용하게 함
+//코스추천에서 지역만을 받는게 있고 contentTyprId/ cat2를 받아야하는게 있는데 어떤식으로 구성할지 고려해야 함
