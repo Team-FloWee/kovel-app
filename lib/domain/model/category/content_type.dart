0f4e92d1e2b628522adc4484a003260b73bb65a0
chojja7188 <shoun7188@naver.com>
2024-05-03 10:36:55 +0900

    refactor: enum을 대체할 카테고리 클래스 작성

---
+++ lib/domain/model/category/content_type.dart
@@ -0,0 +1,33 @@
+import 'package:kovel_app/domain/model/category/category.dart';
+
+class ContentType extends Category {
+  final int contentTypeId;
+  ContentType({required this.contentTypeId}) : super(id: contentTypeId.toString());
+
+  @override
+  String get name => switch (contentTypeId)  {
+    12 => '관광지',
+    14 => '문화시설',
+    15 => '축제공연행사',
+    25 => '여행코스',
+    28 => '레포츠',
+    32 => '숙박',
+    38 => '쇼핑',
+    39 => '음식점',
+    _ => '전체'
+  };
+}
+
+class ContentTypeList {
+  static final typeList = [
+    ContentType(contentTypeId: 0),
+    ContentType(contentTypeId: 12),
+    ContentType(contentTypeId: 14),
+    ContentType(contentTypeId: 15),
+    // ContentType(contentTypeId: 25),
+    ContentType(contentTypeId: 28),
+    ContentType(contentTypeId: 32),
+    ContentType(contentTypeId: 38),
+    ContentType(contentTypeId: 39),
+  ];
+}
