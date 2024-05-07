54ef53aa9e89707427e470d7a8ab34018382a629
itbebop <it.bebop@gmail.com>
2024-05-07 19:41:57 +0900

    feat: 진행중인 축제 모음
    - 크기 변경, 글씨 잘보이도록 뒷그림자 추가

--- lib/presentation/home/components/ongoing_festivals.dart
+++ lib/presentation/home/components/ongoing_festivals.dart
@@ -23,8 +23,8 @@ class OngoingFestivals extends StatelessWidget {
                 ),
               ),
 
-              height: 200, // 내용의 높이
-              width: 200,
+              height: 350, // 내용의 높이
+              width: 350,
             ),
           ),
           Positioned(
@@ -36,14 +36,26 @@ class OngoingFestivals extends StatelessWidget {
             ),
           ),
           Positioned(
-              bottom: MediaQuery.of(context).size.height * 0.033,
+            bottom: MediaQuery.of(context).size.height * 0,
+            left: MediaQuery.of(context).size.width * 0,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
+                color: UiConfig.black.shade900.withOpacity(0.5),
+              ),
+              height: MediaQuery.of(context).size.height * 0.05,
+              width: MediaQuery.of(context).size.width * 1,
+            ),
+          ),
+          Positioned(
+              bottom: MediaQuery.of(context).size.height * 0.029,
               left: MediaQuery.of(context).size.width * 0.04,
               child: Text(
                 area,
                 style: UiConfig.smallStyle.copyWith(color: UiConfig.black.shade100),
               )),
           Positioned(
-            bottom: MediaQuery.of(context).size.height * 0.01,
+            bottom: MediaQuery.of(context).size.height * 0.006,
             left: MediaQuery.of(context).size.width * 0.04,
             child: Text(
               title,
