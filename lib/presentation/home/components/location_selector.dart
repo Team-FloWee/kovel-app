24904dfe1a0b3aebc919d4df6b7ca03fa4f33e4b
itbebop <it.bebop@gmail.com>
2024-05-07 22:46:47 +0900

    feat: 내 주변 관광정보 수정
    - area type으로 title 변경

--- lib/presentation/home/components/location_selector.dart
+++ lib/presentation/home/components/location_selector.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:kovel_app/config/ui_config.dart';
+import 'package:kovel_app/domain/model/category/category.dart';
 
 class LocationSelector extends StatelessWidget {
-  const LocationSelector({super.key});
+  final Category category;
+  final void Function(Category category)? onSelect;
+
+  const LocationSelector({
+    super.key,
+    required this.onSelect,
+    required this.category,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.blue,
+    return GestureDetector(
+      onTap: () {
+        onSelect?.call(category);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: UiConfig.black.shade500,
+        ),
+        padding: const EdgeInsets.all(8),
+        child: Text(
+          category.name,
+          style: UiConfig.smallStyle.copyWith(fontWeight: UiConfig.semiBoldFont),
+          textAlign: TextAlign.center,
+        ),
       ),
-      padding: const EdgeInsets.all(8),
-      height: 20,
-      width: 30,
     );
   }
 }
