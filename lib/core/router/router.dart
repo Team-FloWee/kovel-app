2596cba53fb07b07ddf14c395a708b8c1dcdff34
Yejin.Shin <mingmi_27@naver.com>
2024-05-07 17:46:44 +0900

    feat: edit router

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:kovel_app/data/data_source/tour_info_data_source_impl.dart';
-import 'package:kovel_app/data/repository_impl/unified_detail_repositorty_impl.dart';
+import 'package:kovel_app/domain/use_case/get_common_data_use_case.dart';
+import 'package:kovel_app/domain/use_case/get_detail_data_use_case.dart';
+import 'package:kovel_app/domain/use_case/get_info_data_use_case.dart';
 import 'package:kovel_app/presentation/course/course_info_screen.dart';
 import 'package:kovel_app/presentation/course/course_info_view_model.dart';
 import 'package:kovel_app/presentation/course_list/course_list_screen.dart';
@@ -12,8 +14,6 @@ import 'package:kovel_app/presentation/location_list/location_list_view_model.da
 import 'package:provider/provider.dart';
 
 import '../../data/repository_impl/tour_info_repository_impl.dart';
-import '../../presentation/home/home_screen.dart';
-import '../../presentation/home/home_view_model.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/locationList',
@@ -36,7 +36,6 @@ final goRouter = GoRouter(
       path: '/detail',
       name: 'detail',
       builder: (context, state) {
-
         final id = int.parse(state.uri.queryParameters['id']!);
         final contentTypeId =
             int.parse(state.uri.queryParameters['contentTypeId']!);
@@ -44,8 +43,20 @@ final goRouter = GoRouter(
 
         return ChangeNotifierProvider(
           create: (context) => DetailScreenViewModel(
-            unifiedDetailRepository: UnifiedDetailRepositoryImpl(
-              tourInfoDataSource: TourInfoDataSourceImpl(),
+            getCommonDataUseCase: GetCommonDataUseCase(
+              tourInfoRepository: TourInfoRepositoryImpl(
+                tourInfoDataSource: TourInfoDataSourceImpl(),
+              ),
+            ),
+            getDetailDataUseCase: GetDetailDataUseCase(
+              tourInfoRepository: TourInfoRepositoryImpl(
+                tourInfoDataSource: TourInfoDataSourceImpl(),
+              ),
+            ),
+            getInfoDataUseCase: GetInfoDataUseCase(
+              tourInfoRepository: TourInfoRepositoryImpl(
+                tourInfoDataSource: TourInfoDataSourceImpl(),
+              ),
             ),
           ),
           child:
@@ -56,7 +67,6 @@ final goRouter = GoRouter(
     GoRoute(
       path: '/locationList',
       builder: (context, state) {
-
         return ChangeNotifierProvider(
           create: (context) => LocationListViewModel(
             tourInfoRepository: TourInfoRepositoryImpl(
