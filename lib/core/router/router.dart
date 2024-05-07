199e9ad4792b1fd0f30dcb99897d6ee77e13180d
chojja7188 <shoun7188@naver.com>
2024-05-08 00:42:26 +0900

    feat: Detail 화면 DI 작성

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,8 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:kovel_app/data/data_source/tour_info_data_source_impl.dart';
-import 'package:kovel_app/domain/use_case/get_common_data_use_case.dart';
-import 'package:kovel_app/domain/use_case/get_detail_data_use_case.dart';
-import 'package:kovel_app/domain/use_case/get_info_data_use_case.dart';
+import 'package:kovel_app/di/di_setup.dart';
 import 'package:kovel_app/presentation/course/course_info_screen.dart';
 import 'package:kovel_app/presentation/course/course_info_view_model.dart';
 import 'package:kovel_app/presentation/course_list/course_list_screen.dart';
@@ -42,23 +40,7 @@ final goRouter = GoRouter(
         final title = state.uri.queryParameters['title']!;
 
         return ChangeNotifierProvider(
-          create: (context) => DetailScreenViewModel(
-            getCommonDataUseCase: GetCommonDataUseCase(
-              tourInfoRepository: TourInfoRepositoryImpl(
-                tourInfoDataSource: TourInfoDataSourceImpl(),
-              ),
-            ),
-            getDetailDataUseCase: GetDetailDataUseCase(
-              tourInfoRepository: TourInfoRepositoryImpl(
-                tourInfoDataSource: TourInfoDataSourceImpl(),
-              ),
-            ),
-            getInfoDataUseCase: GetInfoDataUseCase(
-              tourInfoRepository: TourInfoRepositoryImpl(
-                tourInfoDataSource: TourInfoDataSourceImpl(),
-              ),
-            ),
-          ),
+          create: (context) => getIt<DetailScreenViewModel>(),
           child:
               DetailScreen(id: id, contentTypeId: contentTypeId, title: title),
         );
