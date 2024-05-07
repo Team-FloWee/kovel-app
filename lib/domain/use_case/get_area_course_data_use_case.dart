58d73284c3ab448b79a4a50141c52f014d279fb4
sseung <sseutta@naver.com>
2024-05-07 23:16:19 +0900

    refactor: usecase 이름 변경,메소드 파라미터 수정
    -- contentTypeId

--- lib/domain/use_case/get_area_course_data_use_case.dart
+++ lib/domain/use_case/get_area_course_data_use_case.dart
@@ -2,16 +2,16 @@ import 'package:kovel_app/domain/repository/tour_info_repository.dart';
 
 import '../model/tour.dart';
 
-class GetAreaCourseDataUseCase {
+class GetAreaDataUseCase {
   final TourInfoRepository _tourInfoRepository;
 
-  const GetAreaCourseDataUseCase({
+  const GetAreaDataUseCase({
     required TourInfoRepository tourInfoRepository,
   }) : _tourInfoRepository = tourInfoRepository;
 
-  Future<List<Tour>> execute({required String areaCode, required String cat2}) async {
+  Future<List<Tour>> execute({required String areaCode, required String cat2,required int contentTypeId}) async {
     final result = await _tourInfoRepository.getAreaBasedList(
-        areaCode: areaCode, cat2: cat2, contentTypeId: 25);
+        areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId);
     return result;
   }
 }
