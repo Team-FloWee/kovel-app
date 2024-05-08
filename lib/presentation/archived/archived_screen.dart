2b47eba0e17096a91246c60be040f2afca5c79e1
jungeun272 <hanjungeun272@gmail.com>
2024-05-08 18:33:05 +0900

    feat: create archived_screen

---
+++ lib/presentation/archived/archived_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArchivedScreen extends StatefulWidget {
+  const ArchivedScreen({super.key});
+
+  @override
+  State<ArchivedScreen> createState() => _ArchivedScreenState();
+}
+
+class _ArchivedScreenState extends State<ArchivedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
