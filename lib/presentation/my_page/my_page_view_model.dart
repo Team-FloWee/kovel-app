d0126cb0cccebad12c445516aa3a575c674a56df
Yejin.Shin <mingmi_27@naver.com>
2024-05-08 14:19:29 +0900

    feat: edit viewModel

--- lib/presentation/my_page/my_page_view_model.dart
+++ lib/presentation/my_page/my_page_view_model.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kovel_app/domain/model/user.dart';
 
 class MyPageViewModel with ChangeNotifier {
   final userRef = FirebaseFirestore.instance
@@ -21,55 +21,9 @@ class MyPageViewModel with ChangeNotifier {
   }
 
   void getProfile() async {
-    //instance.me = 유저.토큰
-    //DocumentSnapshot docSnap = userRef.doc(id).get();
-    FirebaseAuth.instance.currentUser?.uid;
+    final User user =
+        await userRef.doc('0FPrtfSJJDjR5xnbXdoK').get().then((s) => s.data()!);
+    print(user.email);
     notifyListeners();
-  }
-}
-
-class User {
-  String userId;
-  String name;
-  String email;
-  String imageUrl;
-
-  //토큰 토큰;
-
-  User({
-    required this.userId,
-    required this.name,
-    required this.email,
-    required this.imageUrl,
-  });
-  Map<String, dynamic> toJson() {
-    return {
-      'userId': this.userId,
-      'name': this.name,
-      'email': this.email,
-      'imageUrl': this.imageUrl,
-    };
-  }
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      userId: json['userId'] as String,
-      name: json['name'] as String,
-      email: json['email'] as String,
-      imageUrl: json['imageUrl'] as String,
-    );
-  }
-
-  String getEmail() {
-    return FirebaseAuth.instance.currentUser?.email ?? '메일 없음';
-  }
-
-  String getNickName() {
-    return FirebaseAuth.instance.currentUser?.displayName ?? '이름 없음';
-  }
-
-  String getProfileImageUrl() {
-    return FirebaseAuth.instance.currentUser?.photoURL ??
-        'https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp';
   }
 }
