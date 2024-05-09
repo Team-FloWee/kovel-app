33c712ce7e958598b4b3a4fa095602882598cb80
Yejin.Shin <mingmi_27@naver.com>
2024-05-09 19:26:44 +0900

    style: remove notused import&code

--- lib/domain/repository/tour_info_repository.dart
+++ lib/domain/repository/tour_info_repository.dart
@@ -1,20 +1,7 @@
-import 'package:kovel_app/domain/model/detail/course/course_detail.dart';
-import 'package:kovel_app/domain/model/detail/course/course_detail_info.dart';
-import 'package:kovel_app/domain/model/detail/culture_location/culture_location_detail.dart';
-import 'package:kovel_app/domain/model/detail/festival/festival_detail.dart';
-import 'package:kovel_app/domain/model/detail/leports/leports_detail.dart';
-import 'package:kovel_app/domain/model/detail/lodgment/lodgment_detail.dart';
-import 'package:kovel_app/domain/model/detail/restaurant/restaurant_detail.dart';
-import 'package:kovel_app/domain/model/detail/shopping/shopping_detail.dart';
 import 'package:kovel_app/domain/model/detail/tour_detail.dart';
-import 'package:kovel_app/domain/model/detail/tourist_spot/tourist_spot_detail.dart';
 import 'package:kovel_app/domain/model/detail/unified_detail.dart';
 import 'package:kovel_app/domain/model/detail/unified_info.dart';
 
-import '../model/detail/culture_location/culture_location_detail_info.dart';
-import '../model/detail/festival/festival_detail_info.dart';
-import '../model/detail/leports/leports_detail_info.dart';
-import '../model/detail/tourist_spot/tourist_spot_detail_info.dart';
 import '../model/tour.dart';
 
 abstract interface class TourInfoRepository {
@@ -53,103 +40,6 @@ abstract interface class TourInfoRepository {
   Future<List<TourDetail>> getDetailCommon({
     int pageNo = 1,
     required int id,
-  });
-
-  // 소개 정보 조회 - 컨텐츠 별 (content detail)
-  // (12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스, 28:레포츠, 32:숙박, 38:쇼핑, 39:음식점)
-
-  // 25: 여행 코스
-  Future<List<CourseDetail>> getCourseDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 14: 문화 시설
-  Future<List<CultureLocationDetail>> getCultureLocationDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 15: 축제 공연 행사
-  Future<List<FestivalDetail>> getFestivalDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 28: 레포츠
-  Future<List<LeportsDetail>> getLeportsDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 32: 숙박
-  Future<List<LodgmentDetail>> getLodgmentDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 39: 음식점
-  Future<List<RestaurantDetail>> getRestaurantDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 38: 쇼핑
-  Future<List<ShoppingDetail>> getShoppingDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 12: 관광지
-  Future<List<TouristSpotDetail>> getTouristSpotDetail({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 반복 정보 조회 - 컨텐츠 별 (content detail info)
-  // (12:관광지, 14:문화시설, 15:축제공연행사, 25:여행코스, 28:레포츠)
-
-  // 25: 여행 코스
-  Future<List<CourseDetailInfo>> getCourseDetailInfo({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 14: 문화 시설
-  Future<List<CultureLocationDetailInfo>> getCultureLocationDetailInfo({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 15: 축제 공연 행사
-  Future<List<FestivalDetailInfo>> getFestivalDetailInfo({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 28: 레포츠
-  Future<List<LeportsDetailInfo>> getLeportsDetailInfo({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
-  });
-
-  // 12: 관광지
-  Future<List<TouristSpotDetailInfo>> getTouristSpotDetailInfo({
-    int pageNo = 1,
-    required int id,
-    required int contentTypeId,
   });
 
   Future<List<UnifiedDetail>> getUnifiedDetail({
