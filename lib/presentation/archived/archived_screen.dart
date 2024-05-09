c487a4be2c7288b4a78fcbd09959160e4c54cb50
jungeun272 <hanjungeun272@gmail.com>
2024-05-09 15:02:47 +0900

    feat: make ArchivedScreen with archived_item

--- lib/presentation/archived/archived_screen.dart
+++ lib/presentation/archived/archived_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kovel_app/presentation/archived/components/archived_item.dart';
 
 class ArchivedScreen extends StatefulWidget {
   const ArchivedScreen({super.key});
@@ -10,6 +11,40 @@ class ArchivedScreen extends StatefulWidget {
 class _ArchivedScreenState extends State<ArchivedScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('나의 보관함'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.location_on_rounded),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 24.0, left: 16, right: 16),
+        child: Align(
+          alignment: Alignment.center,
+          child: GridView.builder(
+            itemCount: 20, // 아이템 개수
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              childAspectRatio: 1,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return ArchivedItem(
+                imagePath: '',
+                title: '타이틀',
+                badgeTitle: '음식점',
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
