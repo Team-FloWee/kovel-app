d1469663d6c551bebe1814bdd1fbca96b63e5ef5
Yejin.Shin <mingmi_27@naver.com>
2024-05-09 14:57:21 +0900

    feat: edit getprofile,update profile

--- lib/presentation/my_page/my_page_view_model.dart
+++ lib/presentation/my_page/my_page_view_model.dart
@@ -4,43 +4,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:kovel_app/core/auth/current_user_service.dart';
 import 'package:kovel_app/domain/model/user.dart';
 
 class MyPageViewModel with ChangeNotifier {
   late User _user;
-  User get user => _user;
-
+  String userId = CurrentUserService().getUserId();
   final userRef = FirebaseFirestore.instance
       .collection('user')
       .withConverter<User>(
           fromFirestore: (snapshot, _) => User.fromJson(snapshot.data()!),
           toFirestore: (snapshot, _) => snapshot.toJson());
-
   final _picker = ImagePicker();
 
-  //setProfile은 추후 로그인 쪽으로 이동할 예정
-  void setProfile() async {
-    DocumentReference userDoc = userRef.doc();
-    await userDoc.set(User(
-            userId: userDoc.id,
-            name: "test",
-            email: "test@test",
-            imageUrl: "imageUrl")
-        .toJson());
-    notifyListeners();
-  }
+  User get user => _user;
 
   void getProfile() async {
-    _user =
-        await userRef.doc('0FPrtfSJJDjR5xnbXdoK').get().then((s) => s.data()!);
+    _user = await userRef.doc(userId).get().then((value) => value.data()!);
     notifyListeners();
-
-    // _user = auth.FirebaseAuth.instance.currentUser as User;
-    // notifyListeners();
   }
 
   void updateName(String name) async {
-    await userRef.doc('0FPrtfSJJDjR5xnbXdoK').update({'name': name});
+    await userRef.doc(userId).update({'name': name});
+    getProfile();
     notifyListeners();
   }
 
@@ -49,17 +35,15 @@ class MyPageViewModel with ChangeNotifier {
     if (xFile != null) {
       //이미지 업로드
       final storageRef = FirebaseStorage.instance.ref();
-      final imageRef =
-          storageRef.child('user/0FPrtfSJJDjR5xnbXdoK/profile/profile.png');
+      final imageRef = storageRef.child('user/${userId}/profile/profile.png');
 
       //이미지 다운로드
       await imageRef.putFile(File(xFile.path));
       final downloadUrl = await imageRef.getDownloadURL();
 
       //업데이트 (이후 currentUser로 바꾸기)
-      await userRef
-          .doc('0FPrtfSJJDjR5xnbXdoK')
-          .update({'imageUrl': downloadUrl});
+      await userRef.doc(userId).update({'imageUrl': downloadUrl});
+      getProfile();
     }
     //업데이트된거 다시 불러오기
     getProfile();
