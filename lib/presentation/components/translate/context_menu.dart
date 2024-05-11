6676cc102a31d826167c90dcfeadb3c1d7e11fa0
sseung <sseutta@naver.com>
2024-05-11 12:33:09 +0900

    feat: context_menu.dart 데모
    --

--- lib/presentation/components/translate/context_menu.dart
+++ lib/presentation/components/translate/context_menu.dart
@@ -2,26 +2,51 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:kovel_app/data/data_source/ai_data_source_impl.dart';
+import 'package:kovel_app/data/repository_impl/ai_repository_impl.dart';
 
-class ContextMenu extends StatelessWidget {
+class ContextMenu extends StatefulWidget {
   final Widget child;
   final String text;
   const ContextMenu({super.key, required this.child, required this.text});
 
+  @override
+  State<ContextMenu> createState() => _ContextMenuState();
+}
+
+class _ContextMenuState extends State<ContextMenu> {
   @override
   Widget build(BuildContext context) {
     return CupertinoContextMenu.builder(
         actions: [
           CupertinoContextMenuAction(
             onPressed: (){
-              Clipboard.setData(ClipboardData(text: text));
+              Clipboard.setData(ClipboardData(text: widget.text));
               context.pop();
             },
             child: Text('Copy'),
             trailingIcon: Icons.copy,
           ),
           CupertinoContextMenuAction(
-              onPressed: (){},
+              onPressed: (){
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context){
+                    return SizedBox(
+                      height: 400,
+                      child: Center(
+                        child: FutureBuilder(
+                          future: AiRepositoryImpl(aiDataSource: AiDataSourceImpl()).getTranslation(request: widget.text, language: 'english'),
+                          builder: (context, snapshot) {
+                            return Text(snapshot.data ?? '');
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                );
+
+              },
               child: Text('Translate'),
               trailingIcon: Icons.translate
           )
@@ -41,7 +66,7 @@ class ContextMenu extends StatelessWidget {
                   borderRadius: BorderRadius.circular(16),
                   child: Padding(
                       padding: padding,
-                      child: child
+                      child: widget.child
                   )
               )
           );
