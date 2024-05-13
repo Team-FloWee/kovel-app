3959a8ce247761613f91891df6a1339a89cda44a
Yejin.Shin <mingmi_27@naver.com>
2024-05-13 12:50:09 +0900

    feat: add User method
    -유저관련 메소드 해당 파일로 이동
    -UserProvider로 이름 변경

--- lib/core/auth/current_user_service.dart
+++ lib/core/auth/current_user_service.dart
@@ -1,7 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'dart:convert';
 
-class CurrentUserService {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:kovel_app/domain/model/archived.dart';
+import 'package:kovel_app/domain/model/user.dart';
+
+class UserProvider {
+  late User user;
+
   final currentUser = auth.FirebaseAuth.instance.currentUser;
+  final _userRef = FirebaseFirestore.instance
+      .collection('user')
+      .withConverter<User>(
+          fromFirestore: (snapshot, _) => User.fromJson(snapshot.data()!),
+          toFirestore: (snapshot, _) => snapshot.toJson());
 
   String getUserId() {
     if (currentUser != null) {
@@ -9,5 +21,37 @@ class CurrentUserService {
     } else {
       return '';
     }
+  }
+
+  Future<User> getUser() async {
+    try {
+      final data = await _userRef
+          .doc(UserProvider().getUserId())
+          .get()
+          .then((s) => s.data()!);
+      user = User.fromJson(data as Map<String, dynamic>);
+    } catch (error) {
+      user = const User(
+        userId: '',
+        name: '',
+        email: '',
+        imageUrl: '',
+        archivedList: [],
+      );
+    }
+    return user;
+  }
+
+  List<Archived> getArchived() {
+    return (jsonDecode(user.archivedList.toString()) as List<dynamic>)
+        .map((e) => Archived.fromJson(e))
+        .toList();
+  }
+
+  Future<void> updateArchived(List<Archived> archivedList) async {
+    print(user.userId);
+    await _userRef
+        .doc(user.userId)
+        .update({'archivedList': archivedList.map((e) => jsonEncode(e))});
   }
 }
