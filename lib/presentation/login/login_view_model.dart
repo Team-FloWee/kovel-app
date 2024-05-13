482591e27451e9c16762513635c6a1a59d23e2e1
Yejin.Shin <mingmi_27@naver.com>
2024-05-13 12:45:03 +0900

    feat: delete getUserUsecase

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -5,7 +5,6 @@ import 'package:kovel_app/core/enum/login_platform.dart';
 import 'package:kovel_app/domain/model/user.dart';
 import 'package:kovel_app/domain/use_case/auth/check_user_duplicated_use_case.dart';
 import 'package:kovel_app/domain/use_case/auth/create_user_use_case.dart';
-import 'package:kovel_app/domain/use_case/auth/get_user_use_case.dart';
 import 'package:kovel_app/domain/use_case/auth/login_use_case.dart';
 import 'package:kovel_app/domain/use_case/auth/logout_use_case.dart';
 
@@ -14,19 +13,16 @@ class LoginViewModel with ChangeNotifier {
   LogoutUseCase _logoutUseCase;
   CheckUserDuplicatedUseCase _checkUserDuplicatedUseCase;
   CreateUserUseCase _createUserUseCase;
-  GetUserUseCase _getUserUseCase;
 
   LoginViewModel(
       {required LoginUseCase loginUseCase,
       required LogoutUseCase logoutUseCase,
       required CheckUserDuplicatedUseCase checkUserDuplicatedUseCase,
-      required CreateUserUseCase createUserUseCase,
-      required GetUserUseCase getUserUsecase})
+      required CreateUserUseCase createUserUseCase})
       : _loginUseCase = loginUseCase,
         _logoutUseCase = logoutUseCase,
         _checkUserDuplicatedUseCase = checkUserDuplicatedUseCase,
-        _createUserUseCase = createUserUseCase,
-        _getUserUseCase = getUserUsecase;
+        _createUserUseCase = createUserUseCase;
 
   User? _user;
   User? get user => _user;
@@ -39,8 +35,6 @@ class LoginViewModel with ChangeNotifier {
     final result = await _checkUserDuplicatedUseCase.execute(id: _user!.userId);
     if (result == true) {
       _isNewUser = false;
-      //유즈케이스 좋아요 불러오기
-      final user = await _getUserUseCase.execute(id: _user!.userId);
     } else {
       await _createUserUseCase.execute(user: _user!);
     }
