aaa43dc96e6b5d66b57df9b00bc3983b2998355a
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-05-13 23:20:10 +0900

    refactor: edit use case for english version
    - 한국이 아닌 api에서 코스 데이터가 들어올 때 처리

--- lib/domain/use_case/get_area_data_use_case.dart
+++ lib/domain/use_case/get_area_data_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:kovel_app/domain/repository/tour_info_repository.dart';
-
+import '../../data/data_source/tour_info_data_source_impl.dart';
 import '../model/tour.dart';
 
 class GetAreaDataUseCase {
@@ -9,8 +9,23 @@ class GetAreaDataUseCase {
     required TourInfoRepository tourInfoRepository,
   }) : _tourInfoRepository = tourInfoRepository;
 
-  Future<List<Tour>> execute({int pageNo = 1, required String areaCode, required String cat2, required int contentTypeId}) async {
-    final result = await _tourInfoRepository.getAreaBasedList(areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId);
+  Future<List<Tour>> execute(
+      {int pageNo = 1,
+      required String areaCode,
+      required String cat2,
+      required int contentTypeId}) async {
+    if (language == 'KorService1') {
+      final result = await _tourInfoRepository.getAreaBasedList(
+          areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId);
+      return result;
+    } else if (language != 'KorService1' && contentTypeId == 25) {
+      contentTypeId = 80;
+      final result = await _tourInfoRepository.getAreaBasedList(
+          areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId);
+      return result;
+    }
+    final result = await _tourInfoRepository.getAreaBasedList(
+        areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId);
     return result;
   }
 }
--- lib/domain/use_case/get_detail_data_use_case.dart
+++ lib/domain/use_case/get_detail_data_use_case.dart
@@ -1,3 +1,4 @@
+import 'package:kovel_app/data/data_source/tour_info_data_source_impl.dart';
 import 'package:kovel_app/domain/model/detail/unified_detail.dart';
 import 'package:kovel_app/domain/repository/tour_info_repository.dart';
 
@@ -8,8 +9,20 @@ class GetDetailDataUseCase {
     required TourInfoRepository tourInfoRepository,
   }) : _tourInfoRepository = tourInfoRepository;
 
-  Future<UnifiedDetail> execute({int pageNo = 1, required int id, required int contentTypeId}) async {
-    final result = await _tourInfoRepository.getUnifiedDetail(id: id, contentTypeId: contentTypeId);
+  Future<UnifiedDetail> execute(
+      {int pageNo = 1, required int id, required int contentTypeId}) async {
+    if (language == 'KorService1') {
+      final result = await _tourInfoRepository.getUnifiedDetail(
+          id: id, contentTypeId: contentTypeId);
+      return result.first;
+    } else if (language != 'KorService1' && contentTypeId == 25) {
+      contentTypeId = 80;
+      final result = await _tourInfoRepository.getUnifiedDetail(
+          id: id, contentTypeId: contentTypeId);
+      return result.first;
+    }
+    final result = await _tourInfoRepository.getUnifiedDetail(
+        id: id, contentTypeId: contentTypeId);
     return result.first;
   }
 }
