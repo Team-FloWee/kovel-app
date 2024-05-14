c211d1b9b26c603287a30ddd3d931d90d811ccdf
chojja7188 <shoun7188@naver.com>
2024-05-15 00:06:48 +0900

    feat: 좋아요 카운트 기능 적용

---
+++ lib/domain/use_case/unlike_tour_use_case.dart
@@ -0,0 +1,11 @@
+import 'package:kovel_app/domain/repository/firebase/liked_tour_repository.dart';
+
+class UnLikeTourUseCase {
+  final LikedTourRepository _likedTourRepository;
+
+  UnLikeTourUseCase({required LikedTourRepository likedTourRepository}) : _likedTourRepository = likedTourRepository;
+
+  Future<void> execute({required int id}) async {
+    await _likedTourRepository.unLikeTour(id: id);
+  }
+}
