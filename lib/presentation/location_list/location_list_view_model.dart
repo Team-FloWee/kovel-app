c22082487f259af61622f6bde470fe4c879bde7a
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-05-15 13:38:34 +0900

    refactor: 페이지네이션 작동 시 카테고리에 해당하는 데이터만 나오는 로직 추가

--- lib/presentation/location_list/location_list_view_model.dart
+++ lib/presentation/location_list/location_list_view_model.dart
@@ -1,12 +1,11 @@
 import 'dart:core';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:kovel_app/domain/model/detail/tour_detail.dart';
-import 'package:kovel_app/domain/model/user.dart';
 import 'package:kovel_app/domain/repository/user_repository.dart';
 import 'package:kovel_app/domain/use_case/get_common_data_use_case.dart';
+import 'package:logger/logger.dart';
 
-import '../../domain/model/archived.dart';
 import '../../domain/model/tour.dart';
 import '../../domain/use_case/get_area_data_use_case.dart';
 
@@ -14,6 +13,7 @@ class LocationListViewModel with ChangeNotifier {
   final GetCommonDataUseCase _getCommonDataUseCase;
   final GetAreaDataUseCase _getAreaDataUseCase;
   final UserRepository _userRepository;
+
   LocationListViewModel({
     required GetCommonDataUseCase getCommonDataUseCase,
     required GetAreaDataUseCase getAreaDataUseCase,
@@ -56,6 +56,14 @@ class LocationListViewModel with ChangeNotifier {
 
   bool get isCommonDataLoading => _isCommonDataLoading;
 
+  int? _selectedCategory;
+
+  int? get selectedCategory => _selectedCategory;
+
+  String? _selectedCourseCategory;
+
+  String? get selectedCourseCategory => _selectedCourseCategory;
+
   //서울 areacode에 있는 contentTypeId :25(여행정보)의 contentId를 받아와야 함
   //추천코스 [전체]
   Future<void> getData(String areaCode) async {
@@ -64,14 +72,17 @@ class LocationListViewModel with ChangeNotifier {
 
     _areaBasedDataList = await _getAreaDataUseCase.execute(
         areaCode: areaCode, cat2: '', contentTypeId: 25);
+
     _areaBasedDataList.forEach((element) async {
       _courseDetailList
           .add(await _getCommonDataUseCase.execute(id: element.id));
     }); //이렇게 쓰면 ID 리스트 필요없을 거 같음
 
     notifyListeners();
+
     await getCommonData(areaCode, contentTypeId);
     notifyListeners();
+
     _isLoading = false;
     notifyListeners();
   }
@@ -80,7 +91,7 @@ class LocationListViewModel with ChangeNotifier {
     notifyListeners();
 
     _areaBasedDataList = await _getAreaDataUseCase.execute(
-        areaCode: areaCode, cat2: '', contentTypeId: 25);
+        areaCode: areaCode, cat2: cat2, contentTypeId: 25);
 
     _courseDetailList = [];
     _areaBasedDataList.forEach((element) async {
@@ -122,20 +133,17 @@ class LocationListViewModel with ChangeNotifier {
     notifyListeners();
 
     _areaBasedDataList = [];
+
     _areaBasedDataList.addAll((await _getAreaDataUseCase.execute(
         areaCode: areaCode,
         cat2: '',
-        contentTypeId: 0,
+        contentTypeId: _selectedCategory ?? 0,
         pageNo: ++_commonPageNo)));
-
-    // for (Tour element in _areaBasedDataList) {
-    //   _tourDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
-    //   notifyListeners();
-    // }
 
     await Future.wait(_areaBasedDataList.map((element) async {
       _tourDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
     }));
+
     notifyListeners();
 
     _isCommonDataLoading = false;
@@ -144,15 +152,24 @@ class LocationListViewModel with ChangeNotifier {
 
   // 추천코스 페이지네이션
   void fetchMoreCourseData(String areaCode) async {
+    Logger().d('fetchMoreCourseData >>>>>>>> start'); // ok
     _isCourseDataLoading = true;
+
+    Logger()
+        .d('_isCourseDataLoading >>>>>>>>> $_isCourseDataLoading'); // true ok
     notifyListeners();
 
+    Logger().d('_selectedCourseCategory $_selectedCourseCategory'); // ok
+
     _areaBasedDataList = [];
+
     _areaBasedDataList.addAll((await _getAreaDataUseCase.execute(
         areaCode: areaCode,
-        cat2: '',
+        cat2: _selectedCourseCategory ?? '',
         contentTypeId: 25,
         pageNo: ++_coursePageNo)));
+
+    print('_selectedCourseCategory >>>>>>>>> $_selectedCourseCategory');
 
     await Future.wait(_areaBasedDataList.map((element) async {
       _courseDetailList
@@ -160,7 +177,35 @@ class LocationListViewModel with ChangeNotifier {
     }));
     notifyListeners();
 
+    print('_courseDetailList >>>>>>>>>>>> \n $_courseDetailList');
+
     _isCourseDataLoading = false;
+    notifyListeners();
+  }
+
+  void selectCategory(int category) {
+    _selectedCategory = category;
+
+    // 카테고리를 선택하면 변수를 초기화
+    _commonPageNo = 1;
+    _tourDetailList = [];
+
+    Logger().i('viewModel -> _selectedCategory  $_selectedCategory');
+    notifyListeners();
+  }
+
+  // 코스 카테고리 선택 시
+  void selectCourseCategory(String courseCategory) {
+    _selectedCourseCategory = courseCategory;
+
+    // 카테고리를 선택하면 변수를 초기화
+    _coursePageNo = 1;
+    _courseDetailList = [];
+
+    Logger()
+        .i('viewModel -> _selectedCourseCategory  $_selectedCourseCategory');
+    Logger().i(
+        'viewModel -> _selectedCourseCategory type  ${_selectedCourseCategory.runtimeType}');
     notifyListeners();
   }
 }
