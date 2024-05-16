23a60412060a308affe8d1e425afdc3f2492a4ea
sseung <sseutta@naver.com>
2024-05-16 18:19:39 +0900

    modify : refactoring networkError enum
    -- 변수 추가

--- lib/core/enum/networkError.dart
+++ lib/core/enum/networkError.dart
@@ -1,4 +1,25 @@
-enum NetworkError{
+enum NetworkError {
   requestTimeout,
+  unauthorized,
+  notFound,
+  serverError,
   unknown,
+}
+
+extension NetworkErrorExtension on NetworkError {
+  String get message {
+    switch (this) {
+      case NetworkError.requestTimeout:
+        return 'Request timed out. Please try again later.';
+      case NetworkError.unauthorized:
+        return 'Unauthorized request. Please check your credentials.';
+      case NetworkError.notFound:
+        return 'Resource not found. Please try a different request.';
+      case NetworkError.serverError:
+        return 'Server error. Please try again later.';
+      case NetworkError.unknown:
+      default:
+        return 'An unknown error occurred. Please try again later.';
+    }
+  }
 }
