a21d51c4ca7e13cbded1f7eb36df05970dca3f82
sseung <sseutta@naver.com>
2024-05-16 18:29:22 +0900

    fix : enum
    -- 모든경우 추가

--- lib/presentation/home/home_view_model.dart
+++ lib/presentation/home/home_view_model.dart
@@ -17,7 +17,9 @@ class HomeViewModel with ChangeNotifier {
   final GetSearchFestivalUseCase _getSearchFestivalUseCase;
   final GetAddressUseCase _addressInfoRepository;
 
-  HomeViewModel({required GetSearchFestivalUseCase getSearchFestivalUseCase, required GetAddressUseCase getAddressUseCase})
+  HomeViewModel(
+      {required GetSearchFestivalUseCase getSearchFestivalUseCase,
+      required GetAddressUseCase getAddressUseCase})
       : _getSearchFestivalUseCase = getSearchFestivalUseCase,
         _addressInfoRepository = getAddressUseCase;
   bool isLoading = false;
@@ -29,6 +31,9 @@ class HomeViewModel with ChangeNotifier {
   List<Tour> onGoingTourList = [];
   List<String> locationList = ['현재 위치']; // TODO:초기값은 firebase연결 후에 이전 연결주소
   List<Map<String, double>> distanceList = [];
+
+
+
   // 내 주변 관광정보
   List<Tour> locationBasedList = []; // TODO: 초기값 firebase연결 후에 이전 받아온 관광정보
 
@@ -65,7 +70,8 @@ class HomeViewModel with ChangeNotifier {
     // 두번 이상 거부하면 계속 거부한 것으로 보고 에러냄
     if (permission == LocationPermission.deniedForever) {
       // Permissions are denied forever, handle appropriately.
-      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
     }
     // When we reach here, permissions are granted and we can
     // continue accessing the position of the device.
@@ -78,23 +84,34 @@ class HomeViewModel with ChangeNotifier {
     /* 위도 경도 가져오기 끝 */
 
     // 위도,경도로 주소 가져오기
-    fetchAddressData(longitude: _longitude!.toString(), latitude: _latitude!.toString());
+    fetchAddressData(
+        longitude: _longitude!.toString(), latitude: _latitude!.toString());
 
     // 내 주변 관광정보 추천
-    fetchLocationBasedList(longitude: _longitude!.toString(), latitude: _latitude!.toString(), radius: radius);
+    fetchLocationBasedList(
+        longitude: _longitude!.toString(),
+        latitude: _latitude!.toString(),
+        radius: radius);
     notifyListeners();
   }
 
   // 위도,경도로 주소 가져오기
-  void fetchAddressData({required String longitude, required String latitude}) async {
+  void fetchAddressData(
+      {required String longitude, required String latitude}) async {
     // 주소 받아옴
-    final dataList = await _addressInfoRepository.execute(longitude: longitude, latitude: latitude);
+    final dataList = await _addressInfoRepository.execute(
+        longitude: longitude, latitude: latitude);
     // 구/신주소 중 데이터가 있는 것을 locationList에 넣음
-    switch(dataList){
+    switch (dataList) {
       case Success<List<Address>, NetworkError>():
-        if (dataList.data.first.roadAddress.addressName != '' && dataList.data.first.oldAddress.addressName != '' && !locationList.contains(dataList.data.first.oldAddress.addressName)) {
+        if (dataList.data.first.roadAddress.addressName != '' &&
+            dataList.data.first.oldAddress.addressName != '' &&
+            !locationList
+                .contains(dataList.data.first.oldAddress.addressName)) {
           locationList.insert(0, dataList.data.first.roadAddress.addressName);
-        } else if (dataList.data.first.oldAddress.addressName != '' && !locationList.contains(dataList.data.first.oldAddress.addressName)) {
+        } else if (dataList.data.first.oldAddress.addressName != '' &&
+            !locationList
+                .contains(dataList.data.first.oldAddress.addressName)) {
           locationList.insert(0, dataList.data.first.oldAddress.addressName);
         }
         // 위치 목록이 2이상 되면(주소 받아오면) 초기값 삭제
@@ -105,41 +122,63 @@ class HomeViewModel with ChangeNotifier {
         // selectedLocation 변수에 새로운 주소 할당
         selectedLocation = locationList.first;
         notifyListeners(); // TODO: await 때문에 notifyListeners()실행 후 주소가져옴. 해결방법은?
-case Error<List<Address>, NetworkError>():
-  {
-    switch (dataList.error) {
-      case NetworkError.requestTimeout:
-       //  TODO:
-      case NetworkError.unknown:
-    //    TODO
-    }
-  }
+      case Error<List<Address>, NetworkError>():
+        {
+          switch (dataList.error) {
+            case NetworkError.requestTimeout:
+            //  TODO:
+            case NetworkError.unknown:
+            //    TODO
+            case NetworkError.unauthorized:
+            //TODO
+            case NetworkError.notFound:
+            //TODO
+            case NetworkError.serverError:
+            //TODO
+          }
+        }
     }
   }
 
   // 내 주변 관광정보
-  void fetchLocationBasedList({required String latitude, required String longitude, required String radius}) async {
-    locationBasedList = await TourInfoRepositoryImpl(tourInfoDataSource: TourInfoDataSourceImpl(dio: Dio())).getLocationBasedList(mapX: longitude, mapY: latitude, radius: radius);
+  void fetchLocationBasedList(
+      {required String latitude,
+      required String longitude,
+      required String radius}) async {
+    locationBasedList = await TourInfoRepositoryImpl(
+            tourInfoDataSource: TourInfoDataSourceImpl(dio: Dio()))
+        .getLocationBasedList(mapX: longitude, mapY: latitude, radius: radius);
     // TODO: address datasource에 null체크 추가 필요함
     // 내 주변 관광정보까지 거리 구하기
     for (int i = 0; i < locationBasedList.length; i++) {
       distanceList.add({
-        locationBasedList[i].id.toString():
-            getDistanceToLocation(lat1: double.parse(longitude), lon1: double.parse(latitude), lat2: double.parse(locationBasedList[i].mapy), lon2: double.parse(locationBasedList[i].mapx)),
+        locationBasedList[i].id.toString(): getDistanceToLocation(
+            lat1: double.parse(longitude),
+            lon1: double.parse(latitude),
+            lat2: double.parse(locationBasedList[i].mapy),
+            lon2: double.parse(locationBasedList[i].mapx)),
       });
     }
     notifyListeners();
   }
 
   // 내 위치부터 관광지까지 거리 구하기
-  double getDistanceToLocation({required double lat1, required double lon1, required double lat2, required double lon2}) {
+  double getDistanceToLocation(
+      {required double lat1,
+      required double lon1,
+      required double lat2,
+      required double lon2}) {
     // 지구 반지름 (km 단위)
     const double earthRadius = 6371.0;
     // 두 지점의 위도와 경도 차이를 라디안으로 변환
     double deltaLat = _toRadians(lat2 - lat1);
     double deltaLon = _toRadians(lon2 - lon1);
     // Haversine 공식
-    double squareRoot = sin(deltaLat / 2) * sin(deltaLat / 2) + cos(_toRadians(lat1)) * cos(_toRadians(lat2)) * sin(deltaLon / 2) * sin(deltaLon / 2);
+    double squareRoot = sin(deltaLat / 2) * sin(deltaLat / 2) +
+        cos(_toRadians(lat1)) *
+            cos(_toRadians(lat2)) *
+            sin(deltaLon / 2) *
+            sin(deltaLon / 2);
     double distance = 2 * atan2(sqrt(squareRoot), sqrt(1 - squareRoot));
     // 최종 거리 계산
     double result = earthRadius * distance;
@@ -156,7 +195,9 @@ case Error<List<Address>, NetworkError>():
     isLoading = true;
     final today = DateTime.now();
     notifyListeners();
-    onGoingTourList = await _getSearchFestivalUseCase.execute(eventStartDate: '20240101', eventEndDate: DateFormat('yyyyMMdd').format(today));
+    onGoingTourList = await _getSearchFestivalUseCase.execute(
+        eventStartDate: '20240101',
+        eventEndDate: DateFormat('yyyyMMdd').format(today));
     isLoading = false;
     notifyListeners();
   }
