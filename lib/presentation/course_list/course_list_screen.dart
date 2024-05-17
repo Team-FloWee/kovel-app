9c0ae049039b8c3868512caf22ea50fb6a0129f6
jungeun272 <hanjungeun272@gmail.com>
2024-05-17 23:35:16 +0900

    feat: CourseRecommendation data binding

--- lib/presentation/course_list/course_list_screen.dart
+++ lib/presentation/course_list/course_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kovel_app/config/ui_config.dart';
 import 'package:kovel_app/domain/model/category/category.dart';
 import 'package:kovel_app/domain/model/category/course_category_type.dart';
@@ -23,7 +24,7 @@ class _CourseListScreenState extends State<CourseListScreen> {
     super.initState();
 
     Future.microtask(
-      () => context.read<CourseListViewModel>().getData(widget.areaCode),
+          () => context.read<CourseListViewModel>().getData(widget.areaCode),
     );
   }
 
@@ -32,59 +33,69 @@ class _CourseListScreenState extends State<CourseListScreen> {
     final viewModel = context.watch<CourseListViewModel>();
     return viewModel.isLoading == true
         ? const Center(
-            child: CircularProgressIndicator(),
-          )
+      child: CircularProgressIndicator(),
+    )
         : Scaffold(
-            appBar: const CommonAppBar(
-              title: '여행코스',
-            ),
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Text(
-                        '추천코스',
-                        style: UiConfig.h3Style.copyWith(
-                          fontWeight: UiConfig.semiBoldFont,
-                          color: UiConfig.black.shade900,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    CategoryList(
-                        categoryData: CourseCategoryTypeList.typeList,
-                        onSelect: (Category category) {
-                          // 카테고리 가져오기
-                          context
-                              .read<CourseListViewModel>()
-                              .getCourseData(widget.areaCode, category.id);
-                        }),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Column(
-                      children: viewModel.courseDetailList
-                          .map((e) => Padding(
-                                padding: const EdgeInsets.only(bottom: 16.0),
-                                child: CourseRecommendation(
-                                  tourDetail: e,
-                                ),
-                              ))
-                          .toList(),
-                    ),
-                  ],
+      appBar: const CommonAppBar(
+        title: '여행코스',
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text(
+                  '추천코스',
+                  style: UiConfig.h3Style.copyWith(
+                    fontWeight: UiConfig.semiBoldFont,
+                    color: UiConfig.black.shade900,
+                  ),
                 ),
               ),
-            ),
-          );
+              const SizedBox(
+                height: 16,
+              ),
+              CategoryList(
+                  categoryData: CourseCategoryTypeList.typeList,
+                  onSelect: (Category category) {
+                    // 카테고리 가져오기
+                    context
+                        .read<CourseListViewModel>()
+                        .getCourseData(widget.areaCode, category.id);
+                  }),
+              const SizedBox(
+                height: 16,
+              ),
+              Column(
+                children: viewModel.courseDetailList
+                    .map((e) =>
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: InkWell(
+                        onTap: () {
+                          context.pushNamed(
+                              'detail', queryParameters: {
+                            'id': e.contentId.toString(),
+                            'contentTypeId': e.contentType.id,
+                            'title': e.title});
+                        },
+                        child: CourseRecommendation(
+                          tourDetail: e,
+                        ),
+                      ),
+                    ))
+                    .toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
