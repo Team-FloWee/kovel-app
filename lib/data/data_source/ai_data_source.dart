9cd0603bdfb7ae7d906ede3ddf481d6f7dcf9804
sseung <sseutta@naver.com>
2024-05-17 21:55:11 +0900

    fix : modify ai data

--- lib/data/data_source/ai_data_source.dart
+++ lib/data/data_source/ai_data_source.dart
@@ -3,5 +3,5 @@ import 'package:kovel_app/core/enum/networkError.dart';
 import 'package:kovel_app/core/result/result.dart';
 
 abstract interface class AiDataSource {
-  Result<Stream<GenerateContentResponse>,NetworkError> chatToAi({required String query});
+  Stream<GenerateContentResponse> chatToAi({required String query});
 }
--- lib/data/data_source/ai_data_source_impl.dart
+++ lib/data/data_source/ai_data_source_impl.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'package:kovel_app/core/enum/networkError.dart';
-import 'package:kovel_app/core/result/result.dart';
 
 import 'ai_data_source.dart';
 
@@ -12,15 +10,10 @@ class AiDataSourceImpl implements AiDataSource {
       model: 'gemini-pro', apiKey: dotenv.get('GEMINI_API_KEY'));
 
   @override
-  Result<Stream<GenerateContentResponse>, NetworkError> chatToAi(
-      {required String query}) {
-    try {
-      final content = [Content.text(query)];
-      final response = _model.generateContentStream(content);
+  Stream<GenerateContentResponse> chatToAi({required String query}) {
+    final content = [Content.text(query)];
+    final response = _model.generateContentStream(content);
 
-      return Result.success(response);
-    } catch (e) {
-      return Result.error(NetworkError.unknown);
-    }
+    return response;
   }
 }
