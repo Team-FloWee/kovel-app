4f6fa7f84a8f332928588860c9c66415d16ca63f
Yejin.Shin <mingmi_27@naver.com>
2024-05-19 03:18:51 +0900

    refactor: user datasource가 userid를 파라미터로 안받게 수정

--- lib/domain/use_case/update_user_name_use_case.dart
+++ lib/domain/use_case/update_user_name_use_case.dart
@@ -3,11 +3,11 @@ import 'package:kovel_app/domain/repository/user_repository.dart';
 class UpdateUserNameUseCase {
   final UserRepository _userRepository;
 
-  UpdateUserNameUseCase({required UserRepository userRepository}) : _userRepository = userRepository;
+  UpdateUserNameUseCase({required UserRepository userRepository})
+      : _userRepository = userRepository;
 
-  Future<bool> execute({required String id, required String name}) async {
-    final user = (await _userRepository.getUser(id: id)).copyWith(name: name);
-    final result = await _userRepository.updateUser(user: user);
+  Future<bool> execute({required String name}) async {
+    final result = await _userRepository.updateUserName(name: name);
     return result;
   }
 }
