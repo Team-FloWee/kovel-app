98b90d9897b7e989c613bbfe3727e20d18c8e76c
Yejin.Shin <mingmi_27@naver.com>
2024-05-19 03:19:34 +0900

    refactor: userRef, userProvider 삭제

--- lib/presentation/my_page/my_page_view_model.dart
+++ lib/presentation/my_page/my_page_view_model.dart
@@ -6,63 +6,65 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:kovel_app/core/utils/language_util.dart';
-import 'package:kovel_app/di/di_setup.dart';
 import 'package:kovel_app/domain/model/user.dart';
-
-import '../../core/auth/user_provider.dart';
+import 'package:kovel_app/domain/repository/user_repository.dart';
 
 class MyPageViewModel with ChangeNotifier {
-  late User _user;
-  User get user => _user;
-  String userId =
-      UserProvider(likeTourUseCase: getIt(), unLikeTourUseCase: getIt())
-          .getUserId();
-  final userRef = FirebaseFirestore.instance
+  final UserRepository _userRepository;
+  final _picker = ImagePicker();
+  final _userId = auth.FirebaseAuth.instance.currentUser!.uid;
+  //Todo uid 가져와도될지 세훈님 문의
+  final _userRef = FirebaseFirestore.instance
       .collection('user')
       .withConverter<User>(
           fromFirestore: (snapshot, _) => User.fromJson(snapshot.data()!),
           toFirestore: (snapshot, _) => snapshot.toJson());
-  final _picker = ImagePicker();
+  MyPageViewModel({
+    required UserRepository userRepository,
+  }) : _userRepository = userRepository;
 
-  void getProfile() async {
-    _user = await userRef.doc(userId).get().then((value) => value.data()!);
-    notifyListeners();
+  Future<void> getProfile() async {
+    await _userRepository.getUser();
   }
 
-  void updateLanguage(String lang) async {
-    LanguageUtil().getLanguage(lang);
-    await userRef.doc(user.userId).update({'language': lang});
+  Future<void> updateLanguage(String lang) async {
+    lang = LanguageUtil().getLanguage(lang);
+    await _userRepository.updateLanguage(lang);
     getProfile();
     notifyListeners();
   }
 
-  void updateName(String name) async {
-    await userRef.doc(userId).update({'name': name});
+  Future<void> updateName(String name) async {
+    await _userRepository.updateUserName(name: name);
     getProfile();
     notifyListeners();
   }
 
-  void updatePhoto() async {
+  Future<void> updatePhoto() async {
+    await _userRepository.updatePhoto();
+    getProfile();
+    notifyListeners();
+  }
+
+  Future<void> updatePhoto2() async {
     XFile? xFile = await _picker.pickImage(source: ImageSource.gallery);
     if (xFile != null) {
       //이미지 업로드
       final storageRef = FirebaseStorage.instance.ref();
-      final imageRef = storageRef.child('user/${userId}/profile/profile.png');
+      final imageRef = storageRef.child('user/${_userId}/profile/profile.png');
 
       //이미지 다운로드
       await imageRef.putFile(File(xFile.path));
       final downloadUrl = await imageRef.getDownloadURL();
 
       //업데이트 (이후 currentUser로 바꾸기)
-      await userRef.doc(userId).update({'imageUrl': downloadUrl});
-      getProfile();
+      await _userRef.doc(_userId).update({'imageUrl': downloadUrl});
     }
-    //업데이트된거 다시 불러오기
     getProfile();
     notifyListeners();
   }
 
-  void logout() async {
-    await auth.FirebaseAuth.instance.signOut();
+  Future<void> logout() async {
+    await _userRepository.signOut();
   }
 }
