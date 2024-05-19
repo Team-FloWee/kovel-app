49672e34e160331f483d436ec752df583e04fa87
sseung <sseutta@naver.com>
2024-05-19 22:39:56 +0900

    feat : apply result
    -- 유즈케이스

--- lib/presentation/course_list/course_list_view_model.dart
+++ lib/presentation/course_list/course_list_view_model.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:kovel_app/core/enum/networkError.dart';
+import 'package:kovel_app/core/result/result.dart';
 import 'package:kovel_app/domain/model/detail/tour_detail.dart';
 import 'package:kovel_app/domain/model/tour.dart';
 import 'package:kovel_app/domain/use_case/get_area_data_use_case.dart';
@@ -23,14 +25,16 @@ class CourseListViewModel with ChangeNotifier {
   final bool _isFavorite = false;
 
   final int _areaCode = 1; //서울, 경기...
-
-  List<Tour> _areaBasedDataList = [];
-  List<Tour> get areaBasedDataList => _areaBasedDataList;
+  //
+  // Result<List<Tour>,NetworkError> _areaBasedDataList = Result.error(NetworkError.unknown);
+  // Result<List<Tour>,NetworkError> get areaBasedDataList => _areaBasedDataList;
 
   List<TourDetail> _courseDetailList = [];
+
   List<TourDetail> get courseDetailList => _courseDetailList;
 
   final List<TourDetail> _tourDetail = [];
+
   List<TourDetail> get tourDetail => _tourDetail;
 
   int get contentTypeId => 0;
@@ -39,12 +43,32 @@ class CourseListViewModel with ChangeNotifier {
     // 지역기반조회 컨텐트 아이디 25 넣고 -> 공통정보조회에 아이디 넣고 -> getCommonData
     _isLoading = true;
     notifyListeners();
-    _areaBasedDataList = await _getAreaDataUseCase.execute(
+    final _areaBasedDataList = await _getAreaDataUseCase.execute(
         areaCode: areaCode, cat2: '', contentTypeId: 25);
 
-    await Future.wait(_areaBasedDataList.map((element) async {
-      _courseDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
-    }));
+    switch (_areaBasedDataList) {
+      case Success<List<Tour>, NetworkError>():
+        await Future.wait(_areaBasedDataList.data.map((element) async {
+          _courseDetailList
+              .add(await _getCommonDataUseCase.execute(id: element.id));
+        }));
+      case Error<List<Tour>, NetworkError>():
+        {
+          switch (_areaBasedDataList.error) {
+            case NetworkError.requestTimeout:
+            //  TODO:
+            case NetworkError.unknown:
+            //    TODO
+            case NetworkError.unauthorized:
+            //TODO
+            case NetworkError.notFound:
+            //TODO
+            case NetworkError.serverError:
+            //TODO
+          }
+        }
+        ;
+    }
 
     _isLoading = false;
     notifyListeners();
@@ -54,12 +78,32 @@ class CourseListViewModel with ChangeNotifier {
     // 지역기반조회 지역, 카테고리 받고 -> 공통정보조회 아이디 받음.
     // _isLoading = true;
     notifyListeners();
-    _areaBasedDataList = await _getAreaDataUseCase.execute(
+    final _areaBasedDataList = await _getAreaDataUseCase.execute(
         areaCode: areaCode, cat2: cat2, contentTypeId: 25);
     _courseDetailList = [];
-    await Future.wait(_areaBasedDataList.map((element) async {
-      _courseDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
-    }));
+    switch (_areaBasedDataList) {
+      case Success<List<Tour>, NetworkError>():
+        await Future.wait(_areaBasedDataList.data.map((element) async {
+          _courseDetailList
+              .add(await _getCommonDataUseCase.execute(id: element.id));
+        }));
+      case Error<List<Tour>, NetworkError>():
+        {
+          switch (_areaBasedDataList.error) {
+            case NetworkError.requestTimeout:
+            //  TODO:
+            case NetworkError.unknown:
+            //    TODO
+            case NetworkError.unauthorized:
+            //TODO
+            case NetworkError.notFound:
+            //TODO
+            case NetworkError.serverError:
+            //TODO
+          }
+        }
+        ;
+    }
     // _isLoading = false;
     notifyListeners();
   }
