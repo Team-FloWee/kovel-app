d82620af7b968b543022e3fbb4036137424226f6
itbebop <it.bebop@gmail.com>
2024-05-19 22:47:05 +0900

    feat: 내주변 관광정보 생성image.pngimage.png

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -26,9 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     Future.microtask(() {
-      context
-          .read<HomeViewModel>()
-          .getProfile(context.read<UserProvider>().user);
+      context.read<HomeViewModel>().getProfile(context.read<UserProvider>().user);
       final viewModel = context.read<HomeViewModel>();
       final userProvider = context.read<UserProvider>();
       viewModel.onFetch(userProvider.user.language);
@@ -67,8 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           padding: const EdgeInsets.symmetric(vertical: 8.0),
                           child: Text(
                             '${'안녕하세요,'.tr()}${userProvider.user.name}${'님!'.tr()}',
-                            style: UiConfig.h4Style
-                                .copyWith(fontWeight: UiConfig.semiBoldFont),
+                            style: UiConfig.h4Style.copyWith(fontWeight: UiConfig.semiBoldFont),
                           ),
                         ),
                       ],
@@ -90,8 +87,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       value: e,
                                       child: Text(
                                         e,
-                                        style: UiConfig.smallStyle.copyWith(
-                                            fontWeight: UiConfig.semiBoldFont),
+                                        style: UiConfig.smallStyle.copyWith(fontWeight: UiConfig.semiBoldFont),
                                       ),
                                     );
                                   },
@@ -113,9 +109,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           children: [
                             Text(
                               '주소 새로고침'.tr(),
-                              style: UiConfig.smallStyle.copyWith(
-                                  fontWeight: UiConfig.semiBoldFont,
-                                  color: UiConfig.black.shade800),
+                              style: UiConfig.smallStyle.copyWith(fontWeight: UiConfig.semiBoldFont, color: UiConfig.black.shade800),
                             ),
                             CustomIconButton(
                               icon: Icons.refresh_rounded,
@@ -130,8 +124,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SizedBox(height: 16),
                     TextFormField(
                       onTap: () {
-                        FocusScope.of(context)
-                            .requestFocus(FocusNode()); // 현재 필드의 포커스를 해제합니다.
+                        FocusScope.of(context).requestFocus(FocusNode()); // 현재 필드의 포커스를 해제합니다.
                         context.pushNamed('search');
                       },
                       onTapOutside: (event) => FocusScope.of(context).unfocus(),
@@ -156,9 +149,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       children: [
                         Text(
                           '내주변 관광정보 추천'.tr(),
-                          style: UiConfig.h3Style.copyWith(
-                              color: UiConfig.black,
-                              fontWeight: UiConfig.semiBoldFont),
+                          style: UiConfig.h3Style.copyWith(color: UiConfig.black, fontWeight: UiConfig.semiBoldFont),
                         ),
                         const Spacer(),
                         DecoratedBox(
@@ -179,10 +170,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       value: e,
                                       child: Text(
                                         e,
-                                        style: UiConfig.smallStyle.copyWith(
-                                            fontWeight: UiConfig.semiBoldFont,
-                                            fontSize: 15.sp,
-                                            color: UiConfig.black.shade100),
+                                        style: UiConfig.smallStyle.copyWith(fontWeight: UiConfig.semiBoldFont, fontSize: 15.sp, color: UiConfig.black.shade100),
                                       ),
                                     );
                                   },
@@ -216,31 +204,23 @@ class _HomeScreenState extends State<HomeScreen> {
                                   (index, e) => MapEntry(
                                     index,
                                     Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.start,
                                       children: [
                                         e.title.length > 10
                                             ? Expanded(
                                                 child: Text(
                                                   '${e.title.substring(0, 10)}...',
-                                                  style: UiConfig.bodyStyle
-                                                      .copyWith(
-                                                          fontWeight: UiConfig
-                                                              .semiBoldFont),
+                                                  style: UiConfig.bodyStyle.copyWith(fontWeight: UiConfig.semiBoldFont),
                                                 ),
                                               )
                                             : Expanded(
                                                 child: Text(
                                                   e.title,
-                                                  style: UiConfig.bodyStyle
-                                                      .copyWith(
-                                                          fontWeight: UiConfig
-                                                              .semiBoldFont),
+                                                  style: UiConfig.bodyStyle.copyWith(fontWeight: UiConfig.semiBoldFont),
                                                 ),
                                               ),
                                         Text(
-                                          viewModel.distanceList[index] ??
-                                              '가까이 있음'.tr(),
+                                          viewModel.distanceList[index] ?? '가까이 있음'.tr(),
                                           style: UiConfig.smallStyle,
                                         ),
                                       ],
@@ -253,31 +233,22 @@ class _HomeScreenState extends State<HomeScreen> {
                         : Column(
                             children: [
                               SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.1,
+                                height: MediaQuery.of(context).size.height * 0.1,
                                 width: MediaQuery.of(context).size.width * 0.8,
-                                child: Image.asset(
-                                    'assets/images/search_icon.png'),
+                                child: Image.asset('assets/images/search_icon.png'),
                               ),
                               Text(
                                 '현재 위치에는 관광정보가 없습니다.'.tr(),
-                                style: UiConfig.smallStyle.copyWith(
-                                    color: UiConfig.black.shade700,
-                                    fontWeight: UiConfig.semiBoldFont),
+                                style: UiConfig.smallStyle.copyWith(color: UiConfig.black.shade700, fontWeight: UiConfig.semiBoldFont),
                               ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.02),
+                              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                             ],
                           ),
-                    SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.025),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.025),
                     Row(children: [
                       Text(
                         '🧐 어디로 여행갈까?'.tr(),
-                        style: UiConfig.h3Style.copyWith(
-                            color: UiConfig.black,
-                            fontWeight: UiConfig.semiBoldFont),
+                        style: UiConfig.h3Style.copyWith(color: UiConfig.black, fontWeight: UiConfig.semiBoldFont),
                       ),
                     ]),
                     SizedBox(height: MediaQuery.of(context).size.height * 0.02),
@@ -293,12 +264,18 @@ class _HomeScreenState extends State<HomeScreen> {
                         (index) => LocationSelector(
                           category: AreaTypeList.typeList[index],
                           onSelect: (Category selectedCategory) {
-                            context.pushNamed(
-                              'locationList',
-                              queryParameters: {
-                                'areaCode': selectedCategory.id.toString()
-                              },
-                            );
+                            if (selectedCategory.id == '99' && viewModel.locationBasedList.isNotEmpty) {
+                              context.push('/nearby', extra: viewModel.locationBasedList);
+                            } else if (selectedCategory.id == '99' && viewModel.locationBasedList.isEmpty) {
+                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                content: Text('내 주변 관광정보가 없습니다.'),
+                              ));
+                            } else {
+                              context.pushNamed(
+                                'locationList',
+                                queryParameters: {'areaCode': selectedCategory.id.toString()},
+                              );
+                            }
                           },
                         ),
                       ),
@@ -306,14 +283,10 @@ class _HomeScreenState extends State<HomeScreen> {
                     SizedBox(height: MediaQuery.of(context).size.height * 0.04),
                     Row(
                       children: [
-                        Text('🔥 가장 인기 있는 명소 Top 10'.tr(),
-                            style: UiConfig.h3Style.copyWith(
-                                color: UiConfig.black,
-                                fontWeight: UiConfig.semiBoldFont)),
+                        Text('🔥 가장 인기 있는 명소 Top 10'.tr(), style: UiConfig.h3Style.copyWith(color: UiConfig.black, fontWeight: UiConfig.semiBoldFont)),
                       ],
                     ),
-                    SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.015),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.015),
                   ],
                 ),
               ),
@@ -322,8 +295,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: SizedBox(
                   height: 348,
                   child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
                       mainAxisSpacing: 8,
                       crossAxisSpacing: 8,
@@ -334,24 +306,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     shrinkWrap: false,
                     itemBuilder: (context, index) {
                       return InkWell(
-                        onTap: () =>
-                            context.pushNamed('detail', queryParameters: {
-                          'id': viewModel.popularTourList[index].contentId
-                              .toString(),
-                          'contentTypeId': viewModel
-                              .popularTourList[index].contentType.contentTypeId
-                              .toString(),
-                          'title':
-                              viewModel.popularTourList[index].contentType.name
+                        onTap: () => context.pushNamed('detail', queryParameters: {
+                          'id': viewModel.popularTourList[index].contentId.toString(),
+                          'contentTypeId': viewModel.popularTourList[index].contentType.contentTypeId.toString(),
+                          'title': viewModel.popularTourList[index].contentType.name
                         }),
                         child: FavoriteImage(
-                          archived: ArchivedUtil.getArchived(
-                              tourDetail: viewModel.popularTourList[index]),
+                          archived: ArchivedUtil.getArchived(tourDetail: viewModel.popularTourList[index]),
                           imageSize: 162,
-                          area: AreaType(
-                                  areaCode:
-                                      viewModel.popularTourList[index].areaCode)
-                              .name,
+                          area: AreaType(areaCode: viewModel.popularTourList[index].areaCode).name,
                           title: viewModel.popularTourList[index].title,
                         ),
                       );
@@ -364,10 +327,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Row(
                   children: [
-                    Text('🎉 진행중인 축제 모음'.tr(),
-                        style: UiConfig.h3Style.copyWith(
-                            color: UiConfig.black,
-                            fontWeight: UiConfig.semiBoldFont)),
+                    Text('🎉 진행중인 축제 모음'.tr(), style: UiConfig.h3Style.copyWith(color: UiConfig.black, fontWeight: UiConfig.semiBoldFont)),
                   ],
                 ),
               ),
@@ -385,20 +345,13 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: List.generate(
                       viewModel.onGoingTourList.length,
                       (index) => OngoingFestivals(
-                        archived: ArchivedUtil.getArchived(
-                            tour: viewModel.onGoingTourList[index]),
+                        archived: ArchivedUtil.getArchived(tour: viewModel.onGoingTourList[index]),
                         tourData: viewModel.onGoingTourList[index],
                         onSelect: (Tour selectedTour) {
                           selectedTour.contentType.contentTypeId;
                           context.pushNamed(
                             'detail',
-                            queryParameters: {
-                              'id': selectedTour.id.toString(),
-                              'contentTypeId': selectedTour
-                                  .contentType.contentTypeId
-                                  .toString(),
-                              'title': selectedTour.contentType.name
-                            },
+                            queryParameters: {'id': selectedTour.id.toString(), 'contentTypeId': selectedTour.contentType.contentTypeId.toString(), 'title': selectedTour.contentType.name},
                           );
                         },
                       ),
@@ -415,8 +368,7 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.only(bottom: 16),
         child: FloatingActionButton.extended(
           elevation: 2,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
           backgroundColor: UiConfig.secondaryColor,
           icon: SizedBox(
               width: 24,
@@ -425,10 +377,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Icons.smart_toy_outlined,
                 color: UiConfig.black.shade800,
               )),
-          label: Text('AI 톡톡'.tr(),
-              style: UiConfig.h4Style.copyWith(
-                  fontWeight: UiConfig.semiBoldFont,
-                  color: UiConfig.black.shade800)),
+          label: Text('AI 톡톡'.tr(), style: UiConfig.h4Style.copyWith(fontWeight: UiConfig.semiBoldFont, color: UiConfig.black.shade800)),
           onPressed: () {
             context.push('/chatBot');
           },
---
+++ lib/presentation/nearby_list/nearby_list_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class NearbyListViewModel with ChangeNotifier {
+  bool isLoading = false;
+  void onFetch(id, String language) {}
+}
