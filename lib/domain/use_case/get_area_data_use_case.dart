fdd7c1aaa12f5c44318839bc217138aa9efd7b0d
sseung <sseutta@naver.com>
2024-05-19 22:25:15 +0900

    feat : apply result
    -- 유즈케이스

--- lib/domain/use_case/get_area_data_use_case.dart
+++ lib/domain/use_case/get_area_data_use_case.dart
@@ -1,3 +1,5 @@
+import 'package:kovel_app/core/enum/networkError.dart';
+import 'package:kovel_app/core/result/result.dart';
 import 'package:kovel_app/domain/repository/tour_info_repository.dart';
 
 import '../model/tour.dart';
@@ -9,8 +11,27 @@ class GetAreaDataUseCase {
     required TourInfoRepository tourInfoRepository,
   }) : _tourInfoRepository = tourInfoRepository;
 
-  Future<List<Tour>> execute({int pageNo = 1, required String areaCode, required String cat2, required int contentTypeId}) async {
+  Future<Result<List<Tour>,NetworkError>> execute({int pageNo = 1, required String areaCode, required String cat2, required int contentTypeId}) async {
     final result = await _tourInfoRepository.getAreaBasedList(areaCode: areaCode, cat2: cat2, contentTypeId: contentTypeId, pageNo: pageNo);
-    return result;
+
+    switch (result) {
+      case Success<List<Tour>, NetworkError>():
+        return Result.success(result.data);
+      case Error<List<Tour>, NetworkError>():
+        {
+          switch (result.error) {
+            case NetworkError.unauthorized:
+              return Result.error(NetworkError.unauthorized);
+            case NetworkError.notFound:
+              return Result.error(NetworkError.notFound);
+            case NetworkError.serverError:
+              return Result.error(NetworkError.serverError);
+            case NetworkError.requestTimeout:
+              return Result.error(NetworkError.requestTimeout);
+            case NetworkError.unknown:
+              return Result.error(NetworkError.unknown);
+          }
+        }
+    }
   }
 }
