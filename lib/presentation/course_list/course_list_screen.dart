d5e89682456e2688a9843450533b485c2cdcfe99
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-05-20 02:10:21 +0900

    feat: 코스 리스트 페이지 네이션 기능 추가

--- lib/presentation/course_list/course_list_screen.dart
+++ lib/presentation/course_list/course_list_screen.dart
@@ -20,83 +20,113 @@ class CourseListScreen extends StatefulWidget {
 }
 
 class _CourseListScreenState extends State<CourseListScreen> {
+  final ScrollController _courseListDataScrollController = ScrollController();
+
   @override
   void initState() {
     super.initState();
 
     Future.microtask(
-          () => context.read<CourseListViewModel>().getData(widget.areaCode),
+      () => context.read<CourseListViewModel>().getData(widget.areaCode),
     );
+
+    Future.microtask(() => _courseListDataScrollController.addListener(() {
+          _onCourseListDataScroll();
+        }));
+  }
+
+  @override
+  void dispose() {
+    _courseListDataScrollController.removeListener(_onCourseListDataScroll);
+    _courseListDataScrollController.dispose();
+    super.dispose();
+  }
+
+  void _onCourseListDataScroll() {
+    if (_courseListDataScrollController.position.pixels ==
+            _courseListDataScrollController.position.maxScrollExtent &&
+        !context.read<CourseListViewModel>().isCourseListDataLoading) {
+      context
+          .read<CourseListViewModel>()
+          .fetchMoreCourseListData(widget.areaCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<CourseListViewModel>();
-    return viewModel.isLoading == true
-        ? const Center(
-      child: CircularProgressIndicator(),
-    )
-        : Scaffold(
+    return Scaffold(
       appBar: CommonAppBar(
         title: '여행코스'.tr(),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Text(
-                  '추천코스'.tr(),
-                  style: UiConfig.h3Style.copyWith(
-                    fontWeight: UiConfig.semiBoldFont,
-                    color: UiConfig.black.shade900,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              CategoryList(
-                  categoryData: CourseCategoryTypeList.typeList,
-                  onSelect: (Category category) {
-                    // 카테고리 가져오기
-                    context
-                        .read<CourseListViewModel>()
-                        .getCourseData(widget.areaCode, category.id);
-                  }),
-              const SizedBox(
-                height: 16,
-              ),
-              Column(
-                children: viewModel.courseDetailList
-                    .map((e) =>
+      body: viewModel.isLoading == true
+          ? const Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              controller: _courseListDataScrollController,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 24),
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 16.0),
-                      child: InkWell(
-                        onTap: () {
-                          context.pushNamed(
-                              'detail', queryParameters: {
-                            'id': e.contentId.toString(),
-                            'contentTypeId': e.contentType.id,
-                            'title': e.title});
-                        },
-                        child: CourseRecommendation(
-                          tourDetail: e,
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        '추천코스'.tr(),
+                        style: UiConfig.h3Style.copyWith(
+                          fontWeight: UiConfig.semiBoldFont,
+                          color: UiConfig.black.shade900,
                         ),
                       ),
-                    ))
-                    .toList(),
+                    ),
+                    const SizedBox(height: 16),
+                    CategoryList(
+                      categoryData: CourseCategoryTypeList.typeList,
+                      onSelect: (Category category) {
+                        viewModel.selectCourseCategory(category.id);
+                        // 카테고리 가져오기
+                        context
+                            .read<CourseListViewModel>()
+                            .getCourseData(widget.areaCode, category.id);
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    Column(
+                      children: viewModel.courseDetailList
+                          .map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.only(bottom: 16.0),
+                              child: InkWell(
+                                onTap: () {
+                                  context.pushNamed(
+                                    'detail',
+                                    queryParameters: {
+                                      'id': e.contentId.toString(),
+                                      'contentTypeId': e.contentType.id,
+                                      'title': e.title
+                                    },
+                                  );
+                                },
+                                child: CourseRecommendation(tourDetail: e),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                    viewModel.isCourseListDataLoading != true
+                        ? const SizedBox()
+                        : const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: UiConfig.primaryColor,
+                              ),
+                            ),
+                          )
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- lib/presentation/course_list/course_list_view_model.dart
+++ lib/presentation/course_list/course_list_view_model.dart
@@ -5,6 +5,9 @@ import 'package:kovel_app/domain/use_case/get_area_data_use_case.dart';
 import 'package:kovel_app/domain/use_case/get_common_data_use_case.dart';
 
 class CourseListViewModel with ChangeNotifier {
+  int _courseListPageNo = 1;
+  final _courseContentTypeId = 25;
+
   final GetAreaDataUseCase _getAreaDataUseCase;
   final GetCommonDataUseCase _getCommonDataUseCase;
 
@@ -21,13 +24,24 @@ class CourseListViewModel with ChangeNotifier {
   bool get isLoading => _isLoading;
 
   List<Tour> _areaBasedDataList = [];
+
   List<Tour> get areaBasedDataList => _areaBasedDataList;
 
   List<TourDetail> _courseDetailList = [];
+
   List<TourDetail> get courseDetailList => _courseDetailList;
 
   final List<TourDetail> _tourDetail = [];
+
   List<TourDetail> get tourDetail => _tourDetail;
+
+  bool _isCourseListDataLoading = false;
+
+  bool get isCourseListDataLoading => _isCourseListDataLoading;
+
+  String? _selectedCourseCategory;
+
+  String? get selectedCourseCategory => _selectedCourseCategory;
 
   int get contentTypeId => 0;
 
@@ -36,10 +50,11 @@ class CourseListViewModel with ChangeNotifier {
     _isLoading = true;
     notifyListeners();
     _areaBasedDataList = await _getAreaDataUseCase.execute(
-        areaCode: areaCode, cat2: '', contentTypeId: 25);
+        areaCode: areaCode, cat2: '', contentTypeId: _courseContentTypeId);
 
     await Future.wait(_areaBasedDataList.map((element) async {
-      _courseDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
+      _courseDetailList
+          .add(await _getCommonDataUseCase.execute(id: element.id));
     }));
 
     _isLoading = false;
@@ -51,12 +66,47 @@ class CourseListViewModel with ChangeNotifier {
     // _isLoading = true;
     notifyListeners();
     _areaBasedDataList = await _getAreaDataUseCase.execute(
-        areaCode: areaCode, cat2: cat2, contentTypeId: 25);
+        areaCode: areaCode, cat2: cat2, contentTypeId: _courseContentTypeId);
     _courseDetailList = [];
     await Future.wait(_areaBasedDataList.map((element) async {
-      _courseDetailList.add(await _getCommonDataUseCase.execute(id: element.id));
+      _courseDetailList
+          .add(await _getCommonDataUseCase.execute(id: element.id));
     }));
     // _isLoading = false;
+    notifyListeners();
+  }
+
+  // 코스 리스트 페이지네이션
+  void fetchMoreCourseListData(String areaCode) async {
+    _isCourseListDataLoading = true;
+    notifyListeners();
+
+    _areaBasedDataList = [];
+    _areaBasedDataList.addAll((await _getAreaDataUseCase.execute(
+      areaCode: areaCode,
+      cat2: _selectedCourseCategory ?? '',
+      contentTypeId: _courseContentTypeId,
+      pageNo: ++_courseListPageNo,
+    )));
+
+    await Future.wait(_areaBasedDataList.map((element) async {
+      _courseDetailList
+          .add(await _getCommonDataUseCase.execute(id: element.id));
+    }));
+    notifyListeners();
+
+    _isCourseListDataLoading = false;
+    notifyListeners();
+  }
+
+  // 코스 리스트 카테고리 선택 시
+  void selectCourseCategory(String courseCategory) {
+    _selectedCourseCategory = courseCategory;
+
+    // 카테고리를 선택하면 변수를 초기화
+    _courseListPageNo = 1;
+    _areaBasedDataList = [];
+
     notifyListeners();
   }
 }
