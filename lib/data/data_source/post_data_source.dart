2e5e2f215da342dcb2057c0348cadde11d404237
chojja7188 <shoun7188@naver.com>
2024-05-27 22:34:27 +0900

    feat: 기본적인 PostDataSource 작성

--- lib/data/data_source/post_data_source.dart
+++ lib/data/data_source/post_data_source.dart
@@ -1,18 +1,8 @@
 import 'package:kovel_app/domain/model/post.dart';
 
 abstract interface class PostDataSource {
-  Future<void> createPost({
-    required String userId,
-    required String title,
-    required String content,
-    required String createAt
-  });
+  Future<void> createPost({required Post post});
   Future<List<Post>> getPostList();
-  Future<void> updatePost({
-    required String postId,
-    required String title,
-    required String content,
-    required String updateAt
-  });
+  Future<void> updatePost({required String postId, required Post post});
   Future<void> deletePost({required String postId});
 }
