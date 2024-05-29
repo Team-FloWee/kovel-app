3711d928e251633b2f3458f9edb7aab8ab809a7e
Yejin.Shin <mingmi_27@naver.com>
2024-05-29 16:07:30 +0900

    feat: 유저 정보에 넣을 Schedule 모델 생성

---
+++ lib/domain/model/schedule.freezed.dart
@@ -0,0 +1,459 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'schedule.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Schedule _$ScheduleFromJson(Map<String, dynamic> json) {
+  return _Schedule.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Schedule {
+  int get id => throw _privateConstructorUsedError;
+  int get contentTypeId => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  String get address1 => throw _privateConstructorUsedError;
+  String get address2 => throw _privateConstructorUsedError;
+  String get zipCode => throw _privateConstructorUsedError;
+  String get areaCode => throw _privateConstructorUsedError;
+  String get category1 => throw _privateConstructorUsedError;
+  String get category2 => throw _privateConstructorUsedError;
+  String get category3 => throw _privateConstructorUsedError;
+  String get categoryType => throw _privateConstructorUsedError;
+  String get mapx => throw _privateConstructorUsedError;
+  String get mapy => throw _privateConstructorUsedError;
+  String get imagePath => throw _privateConstructorUsedError;
+  String get tel => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ScheduleCopyWith<Schedule> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ScheduleCopyWith<$Res> {
+  factory $ScheduleCopyWith(Schedule value, $Res Function(Schedule) then) =
+      _$ScheduleCopyWithImpl<$Res, Schedule>;
+  @useResult
+  $Res call(
+      {int id,
+      int contentTypeId,
+      String title,
+      String address1,
+      String address2,
+      String zipCode,
+      String areaCode,
+      String category1,
+      String category2,
+      String category3,
+      String categoryType,
+      String mapx,
+      String mapy,
+      String imagePath,
+      String tel});
+}
+
+/// @nodoc
+class _$ScheduleCopyWithImpl<$Res, $Val extends Schedule>
+    implements $ScheduleCopyWith<$Res> {
+  _$ScheduleCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? contentTypeId = null,
+    Object? title = null,
+    Object? address1 = null,
+    Object? address2 = null,
+    Object? zipCode = null,
+    Object? areaCode = null,
+    Object? category1 = null,
+    Object? category2 = null,
+    Object? category3 = null,
+    Object? categoryType = null,
+    Object? mapx = null,
+    Object? mapy = null,
+    Object? imagePath = null,
+    Object? tel = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      contentTypeId: null == contentTypeId
+          ? _value.contentTypeId
+          : contentTypeId // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      address1: null == address1
+          ? _value.address1
+          : address1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      address2: null == address2
+          ? _value.address2
+          : address2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      zipCode: null == zipCode
+          ? _value.zipCode
+          : zipCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      areaCode: null == areaCode
+          ? _value.areaCode
+          : areaCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      category1: null == category1
+          ? _value.category1
+          : category1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      category2: null == category2
+          ? _value.category2
+          : category2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      category3: null == category3
+          ? _value.category3
+          : category3 // ignore: cast_nullable_to_non_nullable
+              as String,
+      categoryType: null == categoryType
+          ? _value.categoryType
+          : categoryType // ignore: cast_nullable_to_non_nullable
+              as String,
+      mapx: null == mapx
+          ? _value.mapx
+          : mapx // ignore: cast_nullable_to_non_nullable
+              as String,
+      mapy: null == mapy
+          ? _value.mapy
+          : mapy // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+      tel: null == tel
+          ? _value.tel
+          : tel // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ScheduleImplCopyWith<$Res>
+    implements $ScheduleCopyWith<$Res> {
+  factory _$$ScheduleImplCopyWith(
+          _$ScheduleImpl value, $Res Function(_$ScheduleImpl) then) =
+      __$$ScheduleImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {int id,
+      int contentTypeId,
+      String title,
+      String address1,
+      String address2,
+      String zipCode,
+      String areaCode,
+      String category1,
+      String category2,
+      String category3,
+      String categoryType,
+      String mapx,
+      String mapy,
+      String imagePath,
+      String tel});
+}
+
+/// @nodoc
+class __$$ScheduleImplCopyWithImpl<$Res>
+    extends _$ScheduleCopyWithImpl<$Res, _$ScheduleImpl>
+    implements _$$ScheduleImplCopyWith<$Res> {
+  __$$ScheduleImplCopyWithImpl(
+      _$ScheduleImpl _value, $Res Function(_$ScheduleImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? contentTypeId = null,
+    Object? title = null,
+    Object? address1 = null,
+    Object? address2 = null,
+    Object? zipCode = null,
+    Object? areaCode = null,
+    Object? category1 = null,
+    Object? category2 = null,
+    Object? category3 = null,
+    Object? categoryType = null,
+    Object? mapx = null,
+    Object? mapy = null,
+    Object? imagePath = null,
+    Object? tel = null,
+  }) {
+    return _then(_$ScheduleImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      contentTypeId: null == contentTypeId
+          ? _value.contentTypeId
+          : contentTypeId // ignore: cast_nullable_to_non_nullable
+              as int,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      address1: null == address1
+          ? _value.address1
+          : address1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      address2: null == address2
+          ? _value.address2
+          : address2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      zipCode: null == zipCode
+          ? _value.zipCode
+          : zipCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      areaCode: null == areaCode
+          ? _value.areaCode
+          : areaCode // ignore: cast_nullable_to_non_nullable
+              as String,
+      category1: null == category1
+          ? _value.category1
+          : category1 // ignore: cast_nullable_to_non_nullable
+              as String,
+      category2: null == category2
+          ? _value.category2
+          : category2 // ignore: cast_nullable_to_non_nullable
+              as String,
+      category3: null == category3
+          ? _value.category3
+          : category3 // ignore: cast_nullable_to_non_nullable
+              as String,
+      categoryType: null == categoryType
+          ? _value.categoryType
+          : categoryType // ignore: cast_nullable_to_non_nullable
+              as String,
+      mapx: null == mapx
+          ? _value.mapx
+          : mapx // ignore: cast_nullable_to_non_nullable
+              as String,
+      mapy: null == mapy
+          ? _value.mapy
+          : mapy // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+      tel: null == tel
+          ? _value.tel
+          : tel // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ScheduleImpl implements _Schedule {
+  const _$ScheduleImpl(
+      {required this.id,
+      required this.contentTypeId,
+      required this.title,
+      required this.address1,
+      required this.address2,
+      required this.zipCode,
+      required this.areaCode,
+      required this.category1,
+      required this.category2,
+      required this.category3,
+      required this.categoryType,
+      required this.mapx,
+      required this.mapy,
+      required this.imagePath,
+      required this.tel});
+
+  factory _$ScheduleImpl.fromJson(Map<String, dynamic> json) =>
+      _$$ScheduleImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final int contentTypeId;
+  @override
+  final String title;
+  @override
+  final String address1;
+  @override
+  final String address2;
+  @override
+  final String zipCode;
+  @override
+  final String areaCode;
+  @override
+  final String category1;
+  @override
+  final String category2;
+  @override
+  final String category3;
+  @override
+  final String categoryType;
+  @override
+  final String mapx;
+  @override
+  final String mapy;
+  @override
+  final String imagePath;
+  @override
+  final String tel;
+
+  @override
+  String toString() {
+    return 'Schedule(id: $id, contentTypeId: $contentTypeId, title: $title, address1: $address1, address2: $address2, zipCode: $zipCode, areaCode: $areaCode, category1: $category1, category2: $category2, category3: $category3, categoryType: $categoryType, mapx: $mapx, mapy: $mapy, imagePath: $imagePath, tel: $tel)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ScheduleImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.contentTypeId, contentTypeId) ||
+                other.contentTypeId == contentTypeId) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.address1, address1) ||
+                other.address1 == address1) &&
+            (identical(other.address2, address2) ||
+                other.address2 == address2) &&
+            (identical(other.zipCode, zipCode) || other.zipCode == zipCode) &&
+            (identical(other.areaCode, areaCode) ||
+                other.areaCode == areaCode) &&
+            (identical(other.category1, category1) ||
+                other.category1 == category1) &&
+            (identical(other.category2, category2) ||
+                other.category2 == category2) &&
+            (identical(other.category3, category3) ||
+                other.category3 == category3) &&
+            (identical(other.categoryType, categoryType) ||
+                other.categoryType == categoryType) &&
+            (identical(other.mapx, mapx) || other.mapx == mapx) &&
+            (identical(other.mapy, mapy) || other.mapy == mapy) &&
+            (identical(other.imagePath, imagePath) ||
+                other.imagePath == imagePath) &&
+            (identical(other.tel, tel) || other.tel == tel));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      contentTypeId,
+      title,
+      address1,
+      address2,
+      zipCode,
+      areaCode,
+      category1,
+      category2,
+      category3,
+      categoryType,
+      mapx,
+      mapy,
+      imagePath,
+      tel);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ScheduleImplCopyWith<_$ScheduleImpl> get copyWith =>
+      __$$ScheduleImplCopyWithImpl<_$ScheduleImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ScheduleImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Schedule implements Schedule {
+  const factory _Schedule(
+      {required final int id,
+      required final int contentTypeId,
+      required final String title,
+      required final String address1,
+      required final String address2,
+      required final String zipCode,
+      required final String areaCode,
+      required final String category1,
+      required final String category2,
+      required final String category3,
+      required final String categoryType,
+      required final String mapx,
+      required final String mapy,
+      required final String imagePath,
+      required final String tel}) = _$ScheduleImpl;
+
+  factory _Schedule.fromJson(Map<String, dynamic> json) =
+      _$ScheduleImpl.fromJson;
+
+  @override
+  int get id;
+  @override
+  int get contentTypeId;
+  @override
+  String get title;
+  @override
+  String get address1;
+  @override
+  String get address2;
+  @override
+  String get zipCode;
+  @override
+  String get areaCode;
+  @override
+  String get category1;
+  @override
+  String get category2;
+  @override
+  String get category3;
+  @override
+  String get categoryType;
+  @override
+  String get mapx;
+  @override
+  String get mapy;
+  @override
+  String get imagePath;
+  @override
+  String get tel;
+  @override
+  @JsonKey(ignore: true)
+  _$$ScheduleImplCopyWith<_$ScheduleImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
