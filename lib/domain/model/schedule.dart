dc9006c938ef261c353d6951cd2831a721547f45
Yejin.Shin <mingmi_27@naver.com>
2024-05-30 23:56:32 +0900

    feat: schedule 모델 생성

--- lib/domain/model/schedule.dart
+++ lib/domain/model/schedule.dart
@@ -7,16 +7,8 @@ part 'schedule.g.dart';
 class Schedule with _$Schedule {
   const factory Schedule({
     required int id,
-    required int contentTypeId,
+    required int contentType,
     required String title,
-    required String address1,
-    required String address2,
-    required String zipCode,
-    required String areaCode,
-    required String category1,
-    required String category2,
-    required String category3,
-    required String categoryType,
     required String mapx,
     required String mapy,
     required String imagePath,
