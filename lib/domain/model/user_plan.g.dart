18469117b018f07bc6e319c6e005e7e6fc8e2c51
Yejin.Shin <mingmi_27@naver.com>
2024-05-30 23:56:48 +0900

    feat: userplan 모델 생성

---
+++ lib/domain/model/user_plan.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_plan.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserPlanImpl _$$UserPlanImplFromJson(Map<String, dynamic> json) =>
+    _$UserPlanImpl(
+      userId: json['userId'] as String,
+      dateList: (json['dateList'] as List<dynamic>)
+          .map((e) => ScheduleDate.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$UserPlanImplToJson(_$UserPlanImpl instance) =>
+    <String, dynamic>{
+      'userId': instance.userId,
+      'dateList': instance.dateList,
+    };
