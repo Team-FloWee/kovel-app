ceee3a8f54d68c8e5e684b1867251514b8a2a1c6
Yejin.Shin <mingmi_27@naver.com>
2024-06-01 22:23:18 +0900

    feat: updateSchedule삭제 -> scheduledatasource로 이동

--- lib/domain/use_case/update_schedule_use_case.dart
+++ lib/domain/use_case/update_schedule_use_case.dart
@@ -2,18 +2,6 @@ import 'package:kovel_app/domain/model/schedule_date.dart';
 import 'package:kovel_app/domain/repository/schedule_list_repository.dart';
 
 class UpdateScheduleUseCase {
-  // final UserRepository _userRepository;
-  //
-  // UpdateScheduleUseCase({
-  //   required UserRepository userRepository,
-  // }) : _userRepository = userRepository;
-  //
-  // Future<void> execute(
-  //     {required String userId, required List<Schedule> scheduleList}) async {
-  //   await _userRepository.updateSchedule(
-  //       userId: userId, scheduleList: scheduleList);
-  // }
-
   final ScheduleListRepository _scheduleListRepository;
 
   UpdateScheduleUseCase({
@@ -21,8 +9,8 @@ class UpdateScheduleUseCase {
   }) : _scheduleListRepository = scheduleListRepository;
 
   Future<void> execute(
-      {required String userId, required List<ScheduleDate> dateList}) async {
+      {required String userId, required List<ScheduleDate> planList}) async {
     await _scheduleListRepository.updateSchedule(
-        userId: userId, dateList: dateList);
+        userId: userId, planList: planList);
   }
 }
